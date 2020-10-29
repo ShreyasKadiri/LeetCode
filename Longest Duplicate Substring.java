e1ad04609abfd35f4a7393699a1a6feefdd8144a
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2020-10-29 15:13:24 +0530

    Update Longest Duplicate Substring.java

--- Longest Duplicate Substring.java
+++ Longest Duplicate Substring.java
@@ -1,57 +1,46 @@
-//Trie + Binary Search O(logn * n * k)
 class Solution {
-    private String S;
     public String longestDupSubstring(String S) {
-    int maxLowest=0;
-    int maxLength=0;
-    TrieNode root = new TrieNode(0, 0);
-    for (int i=1; i+maxLength<S.length(); i++) {
-        int length=addNew(root,i);
-        if (length>maxLength) {
-            maxLength=length;
-            maxLowest=i;
+        int[] nums = new int[S.length()];
+        
+        for(int i=0; i<S.length(); i++) 
+            nums[i] =(int)S.charAt(i)-(int)'a';
+        int a=26;
+        long mod=(long)Math.pow(2,32);
+        int l=1;
+        int r=S.length();
+        int mid;
+        
+        while(l!=r) {
+            mid=l+ (r-l)/2;
+            if(rabinKarp(mid,a,mod,S.length(),nums)!=-1)
+                l=mid+1;
+            else 
+                r=mid;
         }
-    }
-    return S.substring(maxLowest, maxLowest+maxLength);
-}
-
-    
-private boolean isLeaf(TrieNode node) {
-    return node.next==null;
-}
-
-private int getIndex(int i, int depth) {
-    return S.charAt(i+depth) - 'a';
-}
-
-    
-private int addNew(TrieNode node, int i) {
-    int depth=node.depth;
-    if (i+depth==S.length())
-        return depth;
-    
-    if (isLeaf(node)) {
-        node.next = new TrieNode[26];
-        node.next[getIndex(node.i, node.depth)] = new TrieNode(node.i, depth + 1);
+        
+        int low=rabinKarp(l-1, a, mod, S.length(), nums);
+        return low!= -1? S.substring(low,low+l-1) : "";
     }
     
-    int curr=getIndex(i, node.depth);
-    TrieNode x=node.next[curr];
-    if (x==null) {
-        node.next[curr]=new TrieNode(i,depth + 1);
-        return depth;
+    
+    public int rabinKarp(int mid, int a, long mod, int length, int[] nums) {
+        long h=0;
+        for(int i=0; i<mid; i++)
+            h=(h*a +nums[i])%mod;
+        HashSet<Long> set = new HashSet<Long>();
+        set.add(h);
+        long hash=1;
+        
+        for(int i=1; i<=mid; i++)
+            hash=(hash*a) % mod;
+        
+        for(int start=1; start<length-mid+1; start++){
+            h=(h*a- nums[start-1]* hash %mod + mod) %mod;
+            h=(h + nums[start+mid-1])%mod;
+            if(set.contains(h)) 
+                return start;
+            set.add(h);
+        }
+        return-1;
     }
-    return addNew(x,i);
-}
-
-private static class TrieNode {
-    private TrieNode[] next;
-    private int i;
-    private int depth;
-
-    public TrieNode(int i, int depth) {
-        this.i=i;
-        this.depth=depth;
-    }
-}
 }
