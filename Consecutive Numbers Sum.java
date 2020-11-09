c74a2a45af5c6b4e6331b04f6ed99e9d78a68623
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2020-11-09 17:42:51 +0530

    Update Consecutive Numbers Sum.java

--- Consecutive Numbers Sum.java
+++ Consecutive Numbers Sum.java
@@ -1,7 +1,7 @@
 class Solution {
     public int consecutiveNumbersSum(int N) {
     int count=0;
-        for(int k=1; k<sqrt(2*N); k++){
+        for(int k=1; k<Math.sqrt(2*N); k++){
             int numerator=N-(k*(k-1)/2) , denominator=k;
             if(numerator%denominator==0)
                 count++;
