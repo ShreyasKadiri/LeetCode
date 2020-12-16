07d74aa549db3c425b8efbaa603b88d2547e92fe
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2020-12-16 17:54:10 +0530

    Create N ary tree level order traversal.java

---
+++ N ary tree level order traversal.java
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null) {
+            return result;
+        }
+        
+        Queue<Node> queue = new LinkedList<Node>(); 
+        queue.offer(root); 
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> currentList = new ArrayList<Integer>();
+            for(int i=0; i<size; i++){
+                Node currentNode= queue.poll(); 
+                for(Node node: currentNode.children){
+                    queue.add(node);
+                }
+                currentList.add(currentNode.val);
+            }
+            result.add(currentList);
+        }
+        return result;
+    }
+}
