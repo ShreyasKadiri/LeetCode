c8d85d8a34c97578da7d1d597d7adea639bf58e2
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-01-10 16:35:28 +0530

    binary search

---
+++ binary search.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        if(nums.length==0){
+            return -1;
+        }
+        int low = 0;
+        int high = nums.length-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] < target){
+                low = mid + 1;
+            }else if(nums[mid] > target){
+                high = mid -1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
