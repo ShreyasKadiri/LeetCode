6b29812958cf3f93672064d3b3d23a42930dc6c4
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-01-11 19:02:16 +0530

    Add files via upload

---
+++ Least operators to express a number.java
@@ -0,0 +1,31 @@
+/*
+Approach: Use recursion from x and try all possiblities to reach target and then backtrack to count the minimum operators
+*/
+
+class Solution {
+    public int leastOpsExpressTarget(int x, int target) {
+     int pos=0;
+     int neg=0;
+     int k=0;
+     int pos2;
+     int neg2;
+     int curr;
+     while(target>0){
+            curr=target%x;
+            target=target/x;
+            if(k>0) {
+                pos2=Math.min(curr*k+pos,(curr+1)*k+neg);
+                neg2=Math.min((x-curr)*k+pos,(x-curr-1)*k+neg);
+                pos=pos2;
+                neg=neg2;
+            } 
+         else 
+         {
+         pos=curr*2;
+         neg=(x-curr)*2;
+        }
+            k++;
+        }
+        return Math.min(pos,k+neg)-1;   
+    }
+}
