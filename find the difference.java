e2aba75674ceb4dd78c9a4973f3a35d8e1af9356
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-01-14 11:53:13 +0530

    find the difference

---
+++ find the difference.java
@@ -0,0 +1,16 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+     HashMap<Character,Integer> map = new HashMap<Character,Integer>();
+        for(char c:s.toCharArray())
+        map.put(c, map.getOrDefault(c,0)+1);
+    
+    for(char c:t.toCharArray()){
+        if(map.containsKey(c) && map.get(c)==0 || !map.containsKey(c))
+            return c;
+        else
+            map.put(c,map.get(c)-1);
+    }
+    
+    return '!';
+   }
+}
