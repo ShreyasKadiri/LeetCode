13d6844ee3cf4755e5e55db3ec73abc9f01b4ef8
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-02-09 14:57:36 +0530

    changed by calculating maxcandy value

--- Kids With the Greatest Number of Candies.java
+++ Kids With the Greatest Number of Candies.java
@@ -1,9 +1,14 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         List<Boolean> result = new ArrayList<Boolean>();
+        int maxCandy = 0;
         
         for(int i=0; i<candies.length; i++){
-            if(candies[i]+extraCandies >= candies.length){
+            maxCandy = Math.max(maxCandy, candies[i]);
+        }
+        
+        for(int i=0; i<candies.length; i++){
+            if(candies[i]+extraCandies >= maxCandy){
                 result.add(true);
             } else{
                 result.add(false);
