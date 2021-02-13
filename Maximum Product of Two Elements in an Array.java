3ece90ee7283653b622e7f2ac189ef3ad0809f6c
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-02-13 17:52:15 +0530

    linear approach with two maximums

--- Maximum Product of Two Elements in an Array.java
+++ Maximum Product of Two Elements in an Array.java
@@ -4,3 +4,23 @@ class Solution {
         return (nums[nums.length-1] - 1)*(nums[nums.length-2]-1);
     }
 }
+
+
+
+//Linear Approach
+class Solution {
+    public int maxProduct(int[] nums) {
+        int firstMaximum = Integer.MIN_VALUE;
+        int secondMaximum = firstMaximum;
+        
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] >= firstMaximum){
+                secondMaximum = firstMaximum;
+                firstMaximum = nums[i];
+            }else if(nums[i] > secondMaximum){
+                secondMaximum = nums[i];
+            }
+        }
+        return (firstMaximum- 1)*(secondMaximum-1);
+    }
+}
