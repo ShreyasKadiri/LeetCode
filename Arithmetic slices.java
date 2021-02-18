7ad80b283ceb75f42088f9c0c70a203cb3aaf9e3
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-02-18 17:49:53 +0530

    arithmetic progression with dp

---
+++ Arithmetic slices.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] A) {
+            int dp[] = new int[A.length];
+            int result=0;
+            for(int i=2; i<A.length; i++){
+                if(A[i] - A[i-1] == A[i-1]-A[i-2]) {
+                    dp[i]=1+dp[i-1];
+                    result+=dp[i];
+                }
+            }
+        return result;
+    }
+}
