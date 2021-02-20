3a8f0cfe11bd8fbbe52681487752b0439be1d6dc
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-02-20 15:05:43 +0530

    hashmap + issue fixed

--- convert Roman to Integer.java
+++ convert Roman to Integer.java
@@ -1,19 +1,16 @@
 class Solution {
     public int romanToInt(String s) {
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        map.put('I', 1);    map.put('V', 5);  map.put('X', 10);
-         map.put('L', 50); map.put('C', 100);  map.put('M', 500);  map.put('D', 1000);
-                int result =0;
+        Map<Character, Integer> map = new HashMap();
+            map.put('I', 1);  map.put('V', 5); map.put('X', 10); map.put('L', 50); map.put('C', 100); map.put('D', 500); map.put('M', 1000);
 
-        //There are two condititons i.e additive and subtractive cases
-        for(int i=0; i<s.length(); i++){
-            //For subtractive cases like IV, 1X etc
-            if(i>0 && map.get(s.charAt(i)) > map.get(s.charAt(i-1))  )
-            result += map.get(s.charAt(i)) - 2*(map.get(s.charAt(i-1)));
-            //For additive cases
-            else
-            result+=map.get(s.charAt(i));
-        }
-        return result;
+            int result = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (i > 0 && map.get(s.charAt(i)) > map.get(s.charAt(i - 1))) {
+                    result += map.get(s.charAt(i)) - 2 * map.get(s.charAt(i - 1));
+                } else {
+                    result += map.get(s.charAt(i));
+                }
+            }
+            return result;
     }
 }
