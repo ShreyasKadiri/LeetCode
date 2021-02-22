a25757c154de030fc4f69015de1b7b84910c2011
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-02-22 17:17:18 +0530

    count based

--- Number of Good Pairs.java
+++ Number of Good Pairs.java
@@ -11,3 +11,17 @@ class Solution {
         return count;
     }
 }
+
+
+
+//Lineaar Approach
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+       int count[] = new int[101];
+        int result=0;
+        for(int i=0; i<nums.length; i++){
+            result+=count[nums[i]]++;
+        }
+        return result;
+    }
+}
