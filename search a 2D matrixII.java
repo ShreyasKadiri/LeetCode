915cd340aa10d8c0da6f9b4a68e07fc261e0ff60
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-02-23 17:10:25 +0530

    binary search fixed

--- search a 2D matrixII.java
+++ search a 2D matrixII.java
@@ -1,16 +1,15 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int i=0;
-        int j=matrix[0].length;
-        while(i<matrix[0].length && j>=0){
-            if(matrix[i][j] == target){
-                return true;
-            }else if(matrix[i][j] > target){
-                j--;
-            }else{
-                i++;
-            }
-        }
-        return false;
+    int i=0;
+    int j=matrix[0].length-1;
+    while(i<matrix.length && j>=0) {
+        if(target==matrix[i][j])
+            return true;
+        else if(target>matrix[i][j])
+            i++;
+        else
+            j--;
     }
+ return false;
+   }
 }
