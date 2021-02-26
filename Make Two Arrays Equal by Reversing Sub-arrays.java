8b054e8380c8910dd76545b571ceb66c20cffd72
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-02-26 17:44:48 +0530

    Linear Solution

--- Make Two Arrays Equal by Reversing Sub-arrays.java
+++ Make Two Arrays Equal by Reversing Sub-arrays.java
@@ -1,7 +1,25 @@
+//Sorting
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
         Arrays.sort(target);
         Arrays.sort(arr);
         return Arrays.equals(target,arr);
+    }
+}
+
+//Linear Solution
+class Solution {
+    public boolean canBeEqual(int[] target, int[] arr) {
+       int frequency[] = new int[1001];
+        for(int i=0; i<target.length; i++){
+            frequency[target[i]]++;
+            frequency[arr[i]]--;
+        }
+        for(int i=0; i<frequency.length; i++){
+            if(frequency[i]!=0){
+                return false;
+            }
+        }
+        return true;
     }
 }
