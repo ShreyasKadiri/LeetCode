b3e6f39f65e701067148fcc4535c93d36d6d2efe
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-03-02 16:55:49 +0530

    without hashset

--- set mismatch.java
+++ set mismatch.java
@@ -1,22 +1,19 @@
-class Solution {
-    public int[] findErrorNums(int[] nums) {
-        int error[] = new int[2];
-        HashSet<Integer> set = new HashSet<Integer>();
+class Solution{
+    public static int[] findErrorNums(int[] nums) {
+    int[] result= new int[2];
+	int[] count = new int[nums.length+1];
         
-        //Find the number that occurs twice
-        for(int i=0; i<nums.length; i++){
-            if(!set.contains(nums[i])){
-                set.add(nums[i]);
-            }else{
-                error[0] = nums[i];
-            }
-        }
+	for (int i=0; i<nums.length; i++)
+        count[nums[i]]++;
         
-        for(int i=1; i<=nums.length; i++){
-            if(!set.contains(i)){
-                error[1] = i;
-            }
-        }
-        return error;
-    }
+	for (int i=1; i<count.length; i++){
+		if (count[i]==2) {
+          result[0]=i;  
+        } 
+		if (count[i]==0){
+           result[1] = i; 
+        }  
+	  }
+	return result;
+  }
 }
