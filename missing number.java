9def7ade46b5fb29848f6b92806fb1119826e6f3
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-03-03 14:14:37 +0530

    xor to find missing number

---
+++ missing number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+         int xor = 0;
+    for (int i=0; i<nums.length; i++) {
+        xor = xor ^ nums[i];
+    }
+ 
+    for (int i=0; i<=nums.length; i++) {
+        xor=xor^i;
+    }
+    return xor;
+    }
+}
