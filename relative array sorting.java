b1a9ae91ae1dd89ad46526ac1307ff0e18937c6c
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-03-05 19:10:46 +0530

    bucket sorting issue fixed

--- relative array sorting.java
+++ relative array sorting.java
@@ -1,25 +1,30 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        int bucket[] = new int[1001];
-        for(int i=0; i<arr1.length; i++){
-            bucket[i]+=1;
-        }
-        int result[] = new int[arr1.length];
+        // Bucket for 0 <= arr element <= 1000
+        int[] bucket = new int[1001];
         
-        //Lets store it in order of arr2
+        int[] result = new int[arr1.length];
         int index = 0;
-        for(int i=0; i<arr2.length; i++){
-            if(bucket[i]-- > 0){
+        
+        // Fill bucket
+        for(int i : arr1) {
+            bucket[i]++;
+        }
+        
+        // First store result in order of arr2
+        for(int i : arr2) {
+            while(bucket[i]-- > 0) {
                 result[index++] = i;
             }
         }
         
-        
-        //Fill remaining elements
-        for(int i=0; i<1001; i++){
-             while(bucket[i]-- > 0) {
+        // Fill remaining element
+        for(int i = 0; i < 1001; i++){
+            if(bucket[i] > 0) {
+                while(bucket[i]-- > 0) {
                     result[index++] = i;
                 }
+            }
         }
         return result;
     }
