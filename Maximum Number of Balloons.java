60fe9e8710e97b226a1f40482f2b4baebb80360b
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-03-12 13:13:45 +0530

    frequencies count fixed with minimum occurence

--- Maximum Number of Balloons.java
+++ Maximum Number of Balloons.java
@@ -5,6 +5,7 @@ class Solution {
        for(int i=0; i<text.length(); i++){
           frequencyCount[text.charAt(i) - 'a']++;
        } 
+        minimumOccurence =frequencyCount[1];
         minimumOccurence = Math.min(minimumOccurence, frequencyCount[1]); //character b
         minimumOccurence = Math.min(minimumOccurence, frequencyCount[0]); //character a
         minimumOccurence = Math.min(minimumOccurence,frequencyCount[11] / 2); //character 'l' as "balloon" has 2 occurences of l
