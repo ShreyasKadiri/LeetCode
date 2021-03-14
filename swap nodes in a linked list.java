9f1b666077a0fb0f1163ae5f231314558fd5152e
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-03-14 17:35:49 +0530

    two pointer

---
+++ swap nodes in a linked list.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {		
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode first = head, second = head;
+        
+		// Put fast (k-1) nodes after slow
+        for(int i = 0; i < k - 1; ++i)
+            fast = fast.next;
+            
+		// Save the node for swapping
+        first = fast;
+
+		// Move until the end of the list
+        while(fast.next != null) {
+			slow = slow.next;
+            fast = fast.next;
+        }
+        
+        // Save the second node for swapping
+		// Note that the pointer second isn't necessary: we could use slow for swapping as well
+		// However, having second improves readability
+        second = slow;
+		
+		// Swap values
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+        
+        return head;
+    }
+}
