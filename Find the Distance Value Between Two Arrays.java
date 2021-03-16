0a67c6ed735b72d18e5adbbd0ab5a7d23d01000c
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-03-16 17:58:36 +0530

    binary search fixed

--- Find the Distance Value Between Two Arrays.java
+++ Find the Distance Value Between Two Arrays.java
@@ -38,17 +38,19 @@ class Solution {
     }
     
     
-    public int searchNeighbors(int nums[],  int target){
-        int low = 0 ;
-        int high = nums.length-1;
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            if(nums[mid] < target){
-                low = mid +1 ;
-            }else if (nums[mid] > target){
-                high =  mid -1;
+    public static int searchNeighbors(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while(low < high) {
+            int mid = low + (high - low) / 2;
+
+            if(nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid;
             }
         }
         return high;
-    }   
+    }
 }
