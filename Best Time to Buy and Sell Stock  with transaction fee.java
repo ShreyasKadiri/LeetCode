f1c17ec4e3dfb6666c4c49031c6b68e001565611
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-03-16 17:05:51 +0530

    Update Best Time to Buy and Sell Stock  with transaction fee.java

--- Best Time to Buy and Sell Stock  with transaction fee.java
+++ Best Time to Buy and Sell Stock  with transaction fee.java
@@ -26,3 +26,24 @@ class Solution{
         return ossp;
     }
 }
+
+//Approach 2
+
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+        
+        int len = prices.length;
+        int hold = -prices[0];
+        int notHold = 0;
+        
+        for (int i = 1; i < prices.length; i++) {
+            hold = Math.max(hold, notHold - prices[i]);
+            notHold = Math.max(notHold, hold + prices[i] - fee);
+        }
+        
+        return notHold;
+    }
+}
