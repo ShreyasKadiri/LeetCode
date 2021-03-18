c6c68acf13725b6528a165b3970661011b901d93
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-03-18 19:12:19 +0530

    sorting

--- Can Make Arithmetic Progression From Sequence.java
+++ Can Make Arithmetic Progression From Sequence.java
@@ -1,30 +1,12 @@
 class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
-        int firstTerm  = 0;
-        int secondTerm = 0;
-        
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for(int i=0; i<arr.length; i++){
-            map.put(arr[i], map.getOrDefault(arr[i],0)+1);
-        }
-        
-        //Find the firstSmallest and secondSmallest number from array
-        for(int i=0; i<arr.length; i++){
-            firstTerm = Math.min(arr[i], firstTerm); 
-        }
-        
-        for(int i=0; i<arr.length; i++){
-            secondTerm = Math.min(arr[i], secondTerm); 
-        }
-        
-        
+        Arrays.sort(arr);
+        int firstTerm = arr[0];
+        int secondTerm = arr[1];
         int difference = secondTerm - firstTerm;
-        int i=firstTerm;
-        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-            if(!map.containsKey(i+difference)){
+        for(int i=1; i<arr.length; i++){
+            if(arr[i] - arr[i-1]!=difference){
                 return false;
-            }else {
-                i+=difference;
             }
         }
         return true;
