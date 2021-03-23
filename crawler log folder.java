efec53430cdcc69b3f6a0fc65921d9fe5371ec5c
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-03-23 18:23:35 +0530

    depth of logs

--- crawler log folder.java
+++ crawler log folder.java
@@ -1,17 +1,19 @@
 class Solution {
-      public int minOperations(String[] logs) {
-        int result = 0;
-        for (String log : logs) {
-            if (log.equals("../")){
-              result = Math.max(0, --result);  
-            } 
-            else if (log.equals("./")){
-              continue;  
-            } 
-            else {
-                result++;
+        public int minOperations(String[] logs) {
+            int depth = 0;
+            for (String log : logs) {
+                if (log.equals("./")){
+                    continue;
+                } 
+                else if (log.equals("../")) {
+                    depth--;
+                    if (depth<0) {
+                        depth = 0;
+                    }
+                } else {
+                    depth++;
+                }
+            }
+            return depth;
         }
     }
-        return result;
-    }
-}
