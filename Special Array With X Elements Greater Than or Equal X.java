04c0c3af08db1688d16d6a6d87ebe56bc54ed99c
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-03-26 18:47:00 +0530

    linear code updated

--- Special Array With X Elements Greater Than or Equal X.java
+++ Special Array With X Elements Greater Than or Equal X.java
@@ -14,3 +14,23 @@ class Solution {
         return -1;
     }
 }
+
+
+//O(n)
+class Solution {
+    public int specialArray(int[] nums) {
+        int buckets[] = new int[1001];
+        for(int i=0; i<nums.length; i++){
+            buckets[nums[i]]++;
+        }
+            int total = nums.length;        
+        for(int i=0; i<1001; i++){
+            if(i == total){
+                return i;
+            }else {
+                total = total - buckets[i];
+            }
+        }
+        return -1;
+    }
+}
