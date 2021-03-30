aa5bcaf2d9a3ff1871e3f4e590992fa3a417027b
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-03-30 09:53:03 +0530

    binary search

--- intersection of two arrays.java
+++ intersection of two arrays.java
@@ -1,23 +1,34 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        HashSet<Integer> set1 = new HashSet<Integer>();
-        HashSet<Integer> set2 = new HashSet<Integer>();
+        HashSet<Integer> set = new HashSet<Integer>();
         ArrayList<Integer> result = new ArrayList<>();
-        for(int num : nums1){
-            set1.add(num);
-        }
-        
-        for(int num : nums2){
-            set2.add(num);
-        }
-        
-
-        for(int num : set1){
-            if(set2.contains(num)){
-                result.add(num);
+        Arrays.sort(nums2);
+        for(int  num : nums1){
+            if(checkIfPresent(nums2, num)){
+                set.add(num);
             }
         }
+        
+        for(int num : set){
+            result.add(num);
+        }
+        
         return result.stream().mapToInt(i -> i).toArray();
     }
+    
+    public boolean checkIfPresent(int nums[], int num){
+        int low  = 0;
+        int high = nums.length-1;
+        while(low <= high){
+            int mid  = low + (high - low)/2;
+            if(nums[mid] == num){
+                return true;
+            }else if(nums[mid] < num){
+                low = mid + 1;
+            }else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
 }
-
