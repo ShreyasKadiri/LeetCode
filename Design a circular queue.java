8c116a745228d1d23652a03878123cd5636260f5
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-04 19:33:47 +0530

    front  = 0, fixed

--- Design a circular queue.java
+++ Design a circular queue.java
@@ -1,48 +1,49 @@
 class MyCircularQueue {
-    int front = -1;
-    int rear = -1;
-    int queue[];
-    int size = 0;
-    
-    public MyCircularQueue(int k) {
-        queue = new int[k];
-    }
-    
-    public boolean enQueue(int value) {
-        if(!isFull()){
-            rear = (rear + 1) % queue.length;
-            queue[rear] = value;
-            size++;
-            return true;
+        int[] queue;
+        int front = 0;
+        int rear = -1;
+        int size = 0;
+
+        public MyCircularQueue(int k) {
+            queue = new int[k];
         }
-        else return false;
-    }
-    
-    public boolean deQueue() {
-        if(!isEmpty()){
-            front = (front + 1)  % queue.length;
-            size--;
-            return true;
+
+        public boolean enQueue(int value) {
+            if (!isFull()) {
+                rear = (rear + 1) % queue.length;
+                queue[rear] = value;
+                size++;
+                return true;
+            } 
+            return false;
         }
-        else return false;
+
+        public boolean deQueue() {
+            if (!isEmpty()) {
+                front = (front + 1) % queue.length;
+                size--;
+                return true;
+            } 
+            return false;
+        }
+
+        public int Front() {
+             return isEmpty() ? -1 : queue[front];
+        }
+
+        public int Rear() {
+            return isEmpty() ? -1 : queue[rear];
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        public boolean isFull() {
+            return size == queue.length;
+        }
     }
     
-    public int Front() {
-        return isEmpty()  ? -1: queue[front];
-    }
-    
-    public int Rear() {
-        return isEmpty()  ? -1: queue[rear];
-    }
-    
-    public boolean isEmpty() {
-        return size == 0;
-    }
-    
-    public boolean isFull() {
-        return queue.length == size;
-    }
-}
 
 /**
  * Your MyCircularQueue object will be instantiated and called as such:
