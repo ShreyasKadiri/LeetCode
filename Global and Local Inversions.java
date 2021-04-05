e3b886af614bacda56a1fba50545634e0bd8cad0
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-05 17:42:58 +0530

    linear

--- Global and Local Inversions.java
+++ Global and Local Inversions.java
@@ -1,21 +1,12 @@
 class Solution {
     public boolean isIdealPermutation(int[] A) {
-        int globalInversions = 0;
-        int localInversions = 0;
-        
-        for(int i=0; i<A.length; i++){
-            for(int j=i+1; j<A.length; j++){
-                     if(i<j && A[i] > A[j]){
-                         globalInversions++;
-                     }
-                }
-            }
-        
-            for(int i=1; i<A.length; i++){
-                if(A[i]  < A[i-1]){
-                    localInversions++;
-                }
-            }
-        return globalInversions == localInversions;
+        int maximum = 0;
+        for (int i=0; i<A.length-2; i++) {
+            maximum = Math.max(maximum, A[i]);
+            if (maximum > A[i+2]){
+                return false;
+            }          
+        }
+        return true;
     }
 }
