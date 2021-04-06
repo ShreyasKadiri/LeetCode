e21cd26c983283d90ca26fc44e32e65256dc29a1
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-06 15:40:28 +0530

    one liner

--- Minimum Operations to Make Array Equal.java
+++ Minimum Operations to Make Array Equal.java
@@ -1,6 +1,14 @@
+/*
 class Solution {
     public int minOperations(int n) {
         int count = n/2;
         return count*(count + n%2);
+    }
+}
+*/
+
+class Solution {
+    public int minOperations(int n) {
+        return n * n / 4;
     }
 }
