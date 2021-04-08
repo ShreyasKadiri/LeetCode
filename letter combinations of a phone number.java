678f8b74640d9c16a7f9d06be1800f877fe52560
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-08 17:47:15 +0530

    hashmap + backtracking

--- letter combinations of a phone number.java
+++ letter combinations of a phone number.java
@@ -1,39 +1,39 @@
-class Solution
-{
-    public List<String> letterCombinations(String digits) {
-    HashMap<Character, char[]> dict = new HashMap<Character, char[]>();
-    dict.put('2',new char[]{'a','b','c'});
-    dict.put('3',new char[]{'d','e','f'});
-    dict.put('4',new char[]{'g','h','i'});
-    dict.put('5',new char[]{'j','k','l'});
-    dict.put('6',new char[]{'m','n','o'});
-    dict.put('7',new char[]{'p','q','r','s'});
-    dict.put('8',new char[]{'t','u','v'});
-    dict.put('9',new char[]{'w','x','y','z'});
+class Solution{
+    public List<String> letterCombinations(String s) {
+    HashMap<Character, char[]> map = new HashMap<Character, char[]>();
+    map.put('2',new char[]{'a','b','c'});
+    map.put('3',new char[]{'d','e','f'});
+    map.put('4',new char[]{'g','h','i'});
+    map.put('5',new char[]{'j','k','l'});
+    map.put('6',new char[]{'m','n','o'});
+    map.put('7',new char[]{'p','q','r','s'});
+    map.put('8',new char[]{'t','u','v'});
+    map.put('9',new char[]{'w','x','y','z'});
  
     List<String> result = new ArrayList<String>();
-    if(digits==null||digits.length()==0){
+    if(s==null||s.length()==0){
         return result;
     }
  
-    char[] arr = new char[digits.length()];
-    helper(digits, 0, dict, result, arr);
+    char[] digits = new char[s.length()];
+    findCombinations(s, 0, map, result, digits);
  
     return result;
 }
  
-private void helper(String digits, int index, HashMap<Character, char[]> dict, 
-                        List<String> result, char[] arr){
-    if(index==digits.length()){
-        result.add(new String(arr));
+private void findCombinations(String s, int index, HashMap<Character, char[]> map, 
+                        List<String> result, char[] digits){
+    if(index==s.length()){
+        result.add(new String(digits));
         return;
     }
  
-    char number = digits.charAt(index);
-    char[] candidates = dict.get(number);
-    for(int i=0; i<candidates.length; i++){
-        arr[index]=candidates[i];
-        helper(digits, index+1, dict, result, arr);
+    char currentDigit = s.charAt(index);
+    char[] mappings = map.get(currentDigit);
+    
+    for(int i=0; i<mappings.length; i++){
+        digits[index]=mappings[i];
+        findCombinations(s, index+1, map, result, digits);
     }
-}
+  }
 }
