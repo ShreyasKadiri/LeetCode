5032f805a1bb6428f55746045bcdb59cfbd13147
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-12 17:55:06 +0530

    non increasing sort

--- largest perimeter triangle.java
+++ largest perimeter triangle.java
@@ -1,8 +1,24 @@
 class Solution {
     public int largestPerimeter(int[] nums) {
         Arrays.sort(nums);
-        int largestPerimeter = 0;
-        largestPerimeter = nums[nums.length-1] + nums[nums.length-2] + nums[nums.length-3];
-        return largestPerimeter;
+        int perimeter = 0;
+        
+        //Reverse the array
+        int t = 0;
+        int i=0;
+        int j = nums.length-1;
+        while(i<=j){
+            t = nums[i];
+            nums[i] = nums[j];
+            nums[j] = t;
+        }
+        
+        for(int k=0; k<nums.length-2; k++){
+            if(nums[k] < nums[k+1] + nums[k+2]){
+                perimeter  = Math.max(perimeter, nums[k] + nums[k+1] + nums[k+2]);
+                break;
+            }
+        }
+        return perimeter <=0 ? 0 : perimeter;
     }
 }
