67ab02f42dc09e888ebbeaf82eb5b566bb561c93
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-14 17:31:36 +0530

    two pointer

---
+++ partition list.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode lesserNode = new ListNode(0);
+        ListNode greaterNode = new ListNode(0);
+        ListNode greater = greaterNode;
+        ListNode lesser = lesserNode;
+        
+        while(head!=null){
+            if(head.val <= x){
+                lesser.next = head;
+                lesser  = lesser.next;
+            }else {
+                greater.next = head;
+                greater  = greater.next;
+            }
+            head = head.next;
+        }
+        greater.next = null;
+        lesser.next = greater.next;
+        return lesser.next;
+    }
+}
