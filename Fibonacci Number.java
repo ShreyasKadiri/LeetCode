5082559b27687b1806fa3ddd45ea46fe803fbef4
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-15 20:12:31 +0530

    bottom up dp

--- Fibonacci Number.java
+++ Fibonacci Number.java
@@ -45,3 +45,19 @@ class Solution {
         return dp[n];
     }
 }
+
+
+//Bottom Up DP
+class Solution {
+    public int fib(int n) {
+        int dp[] = new int[n+1];
+        if(n<=1){
+            return n;
+        }
+        dp[1] = 1;
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+}
