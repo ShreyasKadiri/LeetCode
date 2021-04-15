ea21272ddd232c727c67018164ffbb91e8cc1456
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-15 19:58:18 +0530

    three variables

--- Fibonacci Number.java
+++ Fibonacci Number.java
@@ -10,3 +10,21 @@ class Solution {
         return fib(n-1) + fib(n-2);
     }
 }
+
+
+//Three variables
+class Solution {
+    public int fib(int n) {
+      if (n<=1) {
+        return n;
+    }
+        int a=0;
+        int b=1;
+        for (int i=0; i<n-1; i++){
+            int c = a + b;
+            a = b;
+            b = c;
+    }
+    return b;
+    }
+}
