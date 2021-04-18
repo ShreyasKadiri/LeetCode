c5f8ff7e2467f7d109dd63b0bf6f3c1344bfdbf5
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-18 13:00:02 +0530

    one pass

---
+++ remove kth node from last in linked list.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution{
+public ListNode removeNthFromEnd(ListNode head, int k) {
+    ListNode start = new ListNode(0);
+    start.next = head;
+    ListNode p = start;
+    ListNode q = start;
+    
+    //Move fast in front so that the gap between slow and fast becomes n
+    for(int i=1; i<=k+1; i++)   {
+        q = q.next;
+    }
+    //Move fast to the end, maintaining the gap
+    while(q != null) {
+        p = p.next;
+        q = q.next;
+    }
+    //Skip the desired node
+    p.next = p.next.next;
+    return start.next;
+    }
+}
