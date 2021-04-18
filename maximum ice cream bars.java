bfd09d55c0bd5848ed338b5c1befca0170a16cd2
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-18 13:36:03 +0530

    sorting

--- maximum ice cream bars.java
+++ maximum ice cream bars.java
@@ -1,9 +1,12 @@
 class Solution {
      public int maxIceCream(int[] costs, int coins) {
         Arrays.sort(costs);
-        for (int i=0; i<costs.length; ++i)
-            if ((coins -= A[i]) < 0)
+        for (int i=0; i<costs.length; i++){
+            coins = coins - costs[i];
+            if (coins < 0){
                 return i;
-        return A.length;
+            }
+        }
+        return costs.length;
     }
 }
