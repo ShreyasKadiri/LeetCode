5d17be94af492394f491cf772ab5a07b70fc96e1
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-19 19:04:17 +0530

    fancy bruteforcing + optimized bruteforcing

--- good count triplets.java
+++ good count triplets.java
@@ -1,3 +1,4 @@
+//O(n3)
 class Solution {
     public int countGoodTriplets(int[] arr, int a, int b, int c) {
         int count = 0;
@@ -13,4 +14,20 @@ class Solution {
         }
         return count;
     }
+}
+
+
+//Optimized: O(n3)
+class Solution {
+  public int countGoodTriplets(int[] arr, int a, int b, int c) {
+      int count =0;
+      for (int i=0; i<arr.length-2; i++)
+		for (int j=i+1; j<arr.length-1; j++)
+			if (Math.abs(arr[i] - arr[j]) <= a)
+				for (int k=j+1; k<arr.length; k++)
+					if (Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[k] - arr[i]) <= c){
+                        count++;
+                    }
+      return count;
+  }
 }
