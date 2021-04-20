e7a1eb0f2261e307f42fe017af19b2c6224e5d67
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-20 18:53:34 +0530

    greedy

---
+++ Minimum Operations to Make the Array Increasing.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int count = 0;
+        for(int i=1; i<nums.length; i++) {
+            if(nums[i] > nums[i-1]){
+                continue;
+            }
+            int difference = nums[i-1] - nums[i] + 1; 
+            count += difference;
+            nums[i] += difference;
+        }
+        return count;
+    }
+}
