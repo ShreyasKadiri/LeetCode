ba1d6c21bc7f9a4466b14ce9af277b3581224442
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-20 18:39:35 +0530

    stack

--- N-ary tree preorder traversal.java
+++ N-ary tree preorder traversal.java
@@ -11,3 +11,23 @@ class Solution {
         return result;
     }
 }
+
+
+//Stack
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> result = new ArrayList<Integer>();
+        if (root == null){
+            return result; 
+        }
+        Stack<Node> stack = new Stack<Node>();
+        stack.add(root);
+        while (!stack.empty()) {
+            root = stack.pop();
+            result.add(root.val);
+            for (int i=root.children.size()-1; i>=0; i--)
+                stack.add(root.children.get(i));
+        }
+        return result;
+    }
+}
