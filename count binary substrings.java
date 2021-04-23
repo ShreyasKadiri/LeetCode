09bdd7b5495afd81b64c289616b98744fa3c9693
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-23 16:47:42 +0530

    fancy bruteforcing

--- count binary substrings.java
+++ count binary substrings.java
@@ -1,10 +1,26 @@
 class Solution {
     public int countBinarySubstrings(String s) {
         int count = 0;
-        while(n!=0){
-            count+=1;
-            n = n & (n-1);
+        int values[] = new int[2];
+        for(int i=0; i<s.length(); i++){
+            for(int j=0; j<s.length(); j++){
+                String currentString = s.substring(i,j);
+                values = countZeroesOnes(currentString);
+                if(values[0] == values[1]){
+                    count++;
+                }
+            }
         }
         return count;
+    }
+    
+    public int[] countZeroesOnes(String currentString){
+        int ones = 0;
+        for(char c : currentString.toCharArray()){
+            if(c == '1'){
+                ones+=1;
+            }
+        }
+        return new int[]{ones, currentString.length()-ones};
     }
 }
