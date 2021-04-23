cf2715169b2125a2aea2d44f3a7dfb25edc0188d
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-23 17:16:51 +0530

    two pointers

--- count binary substrings.java
+++ count binary substrings.java
@@ -24,3 +24,25 @@ class Solution {
         return new int[]{ones, currentString.length()-ones};
     }
 }
+
+
+//Linear
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int currentLength = 1;
+        int previousLength = 0;
+        int count = 0;
+        for (int i=1; i<s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i-1)){
+                currentLength++;
+            }else {
+            previousLength = currentLength;
+            currentLength = 1;
+        }
+        if (previousLength >= currentLength){
+            count++;
+        }
+    }
+    return count;    
+    }
+}
