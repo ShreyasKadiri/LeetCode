9c31c41eaa31c21e31ac7990a0aac2a6678971ef
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-25 16:02:40 +0530

    math

--- sum of digits in base k.java
+++ sum of digits in base k.java
@@ -1,12 +1,11 @@
 class Solution {
     public int sumBase(int n, int k) {
-        String base = Integer.toString(Integer.parseInt(String.valueOf(n),10),k);
-        int number = Integer.parseInt(base);
-        int sum = 0;
-        while(number!=0){
-            number = number % 10;
-            sum+=number;
-            number = number/10;
+        int units=0;
+        int sum=0;
+        while (n>0){
+            units = n%k;
+            sum+=units;
+            n=n/k;
         }
         return sum;
     }
