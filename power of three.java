b5fc700c65617a8dceb84515b21cef4244c9bbc0
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-27 17:45:59 +0530

    upto 31 bits

--- power of three.java
+++ power of three.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        for(int i=0; i<=n; i++){
-            if(Math.pow(3,i) == n){
+        for(int i=0; i<31; i++){
+            if(Math.pow(3, i) == n){
                 return true;
             }
         }
