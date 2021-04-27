8511b7930580ffc0f2b55a71be18bbc2211e6a34
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-27 17:52:55 +0530

    logn approach

--- power of three.java
+++ power of three.java
@@ -9,7 +9,21 @@ class Solution {
     }
 }
 
-//One liner 
+
+//Time complexity O(log(n)), space complexity O(1)
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n < 1) {
+            return false;
+        }
+        while (n%3 == 0) {
+            n/=3;
+        }
+        return n==1;
+    }
+}
+
+//Time complexity: O(1), space complexity: O(1)
 class Solution {
     public boolean isPowerOfThree(int n) {
             return n>0 && 1162261467%n == 0;
