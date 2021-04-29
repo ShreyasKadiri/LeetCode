24bac86a6d59a8f7a9f9a75662d2652b0286ba2d
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-29 17:09:48 +0530

    single pass linear approach

--- find first and last position of an element in an array.java
+++ find first and last position of an element in an array.java
@@ -1,24 +1,16 @@
+//O(n)
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if(nums.length == 1){
-            return new int[] {0,0};
-        }
-        
         int firstIndex = -1;
-        int secondIndex = -1;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] == target){
-                firstIndex = i;
-                break;
-            }
+        int lastIndex = -1;
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i]==target && firstIndex==-1) {
+               firstIndex = i;
+          }
+            if (nums[i]==target && firstIndex!=-1) {
+                 lastIndex = i;
+           }
         }
-        
-        
-        for(int i=nums.length-1; i>firstIndex; i--){
-            if(nums[i] == target){
-                secondIndex = i;
-            }
-        }
-        return new int[]{firstIndex, secondIndex};
+        return new int[]{firstIndex, lastIndex};
     }
 }
