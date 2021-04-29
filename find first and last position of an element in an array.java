0028da2ed9d420d4020ea5c080b38804c1632d9f
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-29 17:17:43 +0530

    binary search

--- find first and last position of an element in an array.java
+++ find first and last position of an element in an array.java
@@ -14,3 +14,52 @@ class Solution {
         return new int[]{firstIndex, lastIndex};
     }
 }
+
+
+//Binary Search : O(nlogn)
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        firstIndex(nums, target);
+        lastIndex(nums, target);
+        return new int[]{firstIndex, lastIndex};
+    }
+    
+    public int firstIndex(int nums[], int target){
+        int firstIndex = -1;
+        int low = 0;
+        int high = nums.length-1;
+        
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target){
+                firstIndex = mid;
+                high = mid - 1;
+            }else if(nums[mid]<target){
+                low = mid + 1;
+            }else {
+                high = mid - 1;
+            }
+        }
+        return firstIndex;
+    }
+    
+    public int lastIndex(int nums[], int target){
+        int lastIndex = -1;
+        int low =0;
+        int high = nums.length-1;
+        while(low <= high){
+        int mid = low + (high - low)/2;
+            if(nums[mid]== target){
+                lastIndex = mid;
+                low = mid+1;
+            }
+            else if(nums[mid]<target){
+                low = mid+1;
+            }
+        else{
+            end = mid-1;
+        }
+    }
+        return lastIndex;
+    }
+}
