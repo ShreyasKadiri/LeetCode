dc7c7ba8bb680467e75251faf8d4be23fc1824cd
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-30 17:07:57 +0530

    set with  math

--- powerful integers.java
+++ powerful integers.java
@@ -15,3 +15,21 @@ class Solution{
         return new ArrayList(set);
     }
 }
+
+
+class Solution {
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
+        Set<Integer> set = new HashSet<Integer>();  
+        int X = (int) (x == 1 ? 0 : Math.log(bound) / Math.log(x));
+        int Y = (int) (y == 1 ? 0 : Math.log(bound) / Math.log(y));
+        for(int i=0; i<=X; i++) {
+            for(int j=0; j<=Y; j++) {
+                int value = (int) (Math.pow(x, i)) + Math.pow(y, j);
+                if(value <= bound){
+                    set.add(value);
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
+}
