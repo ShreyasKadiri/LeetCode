89fca750dc171bc2112bf21f7f9acd5c0172cbcb
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-01 19:30:37 +0530

    trie

--- prefix and suffix search.java
+++ prefix and suffix search.java
@@ -6,44 +6,43 @@ class TrieNode {
         children = new TrieNode[27];
     }
 }
+
 class WordFilter {
     TrieNode root;
     public WordFilter(String[] words) {
         root = new TrieNode();
-        for (int weight = 0; weight < words.length; weight++) {
-            String word = '{' + words[weight];
-            insert(root, word, weight);
-            for (int i = 0; i < word.length(); i++) {
-                //skip first '{' in word
-                insert(root, word.substring(i+1) + word, weight);
+        for (int i=0; i<words.length; i++) {
+            String word = '{' + words[i];
+            insert(root, word, i);
+            for (int j=0; j<word.length(); j++) {
+                insert(root, word.substring(j+1) + word, i);
             }
         }
     }
 
     private void insert(TrieNode root, String word, int weight) {
-        TrieNode cur = root;
+        TrieNode currentNode = root;
         for(char c : word.toCharArray()) {
             int k = c - 'a';
-            if(cur.children[k] == null) {
-                cur.children[k] = new TrieNode();
+            if(currentNode.children[k] == null) {
+                currentNode.children[k] = new TrieNode();
             } 
-            cur = cur.children[k];
-            cur.weight = weight;
+            currentNode = currentNode.children[k];
+            currentNode.weight = weight;
         }
     }
 
     public int f(String prefix, String suffix) {
-        TrieNode cur = root;
+        TrieNode currentNode = root;
         for(char c : (suffix + '{' + prefix).toCharArray()) {
-            if (cur.children[c - 'a'] == null) {
+            if (currentNode.children[c - 'a'] == null) {
                 return -1;
             }
-            cur = cur.children[c-'a'];
+            currentNode = currentNode.children[c-'a'];
         }
-        return cur.weight;
+        return currentNode.weight;
     }
 }
-
 
 /**
  * Your WordFilter object will be instantiated and called as such:
