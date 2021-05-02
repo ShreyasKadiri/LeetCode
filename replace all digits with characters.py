5f17d2677ec6b890f1c668261f0f08bc297e49db
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-02 11:07:29 +0530

    characters

--- replace all digits with characters.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def replaceDigits(self, text:str) -> str:
-        nums = [str(ord(x) - 96) for x in text.lower() if x >= 'a' and x <= 'z']
-        return " ".join(nums)
-        
