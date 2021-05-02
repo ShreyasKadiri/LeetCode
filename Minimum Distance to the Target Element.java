9cc726d8df42ded3fc6ec6a284a12f21440c7dd5
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-02 11:21:09 +0530

    linear

--- Minimum Distance to the Target Element.java
+++ Minimum Distance to the Target Element.java
@@ -1,11 +1,13 @@
 class Solution {
     public int getMinDistance(int[] nums, int target, int start) {
-        int index = -1;
+        int result = Integer.MAX_VALUE;
         for(int i=0; i<nums.length; i++){
             if(nums[i] == target){
-                index  = i;
+                if(Math.abs(start - i) < result){
+                    result = Math.abs(start - i);
+                }
             }
         }
-        return Math.abs(target - index);
+        return result;
     }
 }
