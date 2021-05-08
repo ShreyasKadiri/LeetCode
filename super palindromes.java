fdc4cb91ce1260041a67a04d05aadaf22cf2df9e
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-08 17:47:14 +0530

    dfs on strings

--- super palindromes.java
+++ super palindromes.java
@@ -1,24 +1,39 @@
-class Solution {
+class Solution{
     public int superpalindromesInRange(String left, String right) {
-        int count = 0;
-        int low = Integer.parseInt(left);
-        int high = Integer.parseInt(right);
-        for(int i=low; i<=high; i++){
-            if(checkPalindrome(i) && checkPalindrome(Math.sqrt(i))){
-                    count++;
-            }
-        }
+        int low = (int)Math.ceil(Math.sqrt(Long.parseLong(left)));
+        int high = (int)Math.floor(Math.sqrt(Long.parseLong(right)));
+        int count = (3 >= low && 3 <= high) ? 1 : 0;
+        count += dfs(low, high, "");
+	    count += dfs(low, high, "0");
+	    count += dfs(low, high, "1");
+	    count += dfs(low, high, "2");
         return count;
     }
-       
-    public boolean checkPalindrome(double n){
-        double temp=n;
-        double reverse = 0;
-        double remainder = 0;
-        while(n > 0){
-            reverse = (reverse*10) + n%10;    
-            n = n / 10;    
+    
+    private int dfs(int low, int high, String string) {
+	if (string.length() > Integer.toString(high).length()) {
+        return 0;
+    }
+        
+	int count = 0;
+	if (!string.isEmpty() && string.charAt(0) != '0') {
+		long number = Long.parseLong(string);
+		if (number > high){
+            return 0;
         }
-        return reverse == temp;
+		if (number >= low && isPalindrome(number * number)){
+            count++;
+        }
+	}
+
+	for (char c='0'; c<='2'; c++) {
+		count += dfs(low, high, c+string+c);
+	}
+	return count;
+    }
+    
+    private boolean isPalindrome(long number) {
+        String string = Long.toString(number);
+        return string.equals(new StringBuilder(string).reverse().toString());
     }
 }
