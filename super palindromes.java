06edfbfaf64807a333a1c5fb6de83009824d0d62
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-08 17:23:41 +0530

    all possibilities

---
+++ super palindromes.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int superpalindromesInRange(String left, String right) {
+        int count = 0;
+        int low = Integer.parseInt(left);
+        int high = Integer.parseInt(right);
+        for(int i=low; i<=high; i++){
+            if(checkPalindrome(i)){
+                if(checkPalindrome(Math.sqrt(i))){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+       
+    public boolean checkPalindrome(double n){
+        double temp=n;
+        double reverse = 0;
+        double remainder = 0;
+        while(n > 0){
+            reverse = (reverse*10) + n%10;    
+            n = n / 10;    
+        }
+        return reverse == temp;
+    }
+}
