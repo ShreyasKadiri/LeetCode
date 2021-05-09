2cf15026de6a426f6f0640b5be8106fa3183c87c
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-09 16:07:01 +0530

    priority queue

--- construct array with multiple sums.java
+++ construct array with multiple sums.java
@@ -1,24 +1,24 @@
 class Solution {
-    public boolean isPossible(int[] target) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>((a,b) -> (b-a));
-        int total = 0;
-        for(int i=0; i<target.length; i++){
-            total+= target[i];
-            maxHeap.add(total);
+     public boolean isPossible(int[] target) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>((a, b) -> (b - a));
+        long total = 0;
+        for (int i=0; i<target.length; i++) {
+            total += target[i];
+            maxHeap.add(target[i]);
         }
-        while(!maxHeap.isEmpty()){
-            int popped = maxHeap.poll();
-            total-=popped;
-            if(popped == 1 || total == 1){
-                return true;
-            }
-            if(total ==0 || popped<total || popped%total == 0){
-                return false;
-            }
-            popped = popped % total;
-            total = total + popped;
-            maxHeap.add(popped);
-        }
-        return true;
-    }
+         
+         while (true) {
+             int popped = maxHeap.poll();
+             total = total - popped;
+             if (popped == 1 || total == 1){
+                 return true;
+             }
+             if (popped < total || total == 0 ||  popped% total == 0){
+                 return false;
+             }
+             popped%= total;
+             total = total + popped;
+             maxHeap.add(popped);
+         }
+     }
 }
