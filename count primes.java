64b5f1e7b00f63b1c47332dbfb0875f028d491c6
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-10 17:26:21 +0530

    brute force + sieve

--- count primes.java
+++ count primes.java
@@ -1,3 +1,30 @@
+//Brute Force
+
+class Solution{
+    public int countPrimes(int n){
+        if(n < 3){
+            return 0;
+        }
+        int count = 1;
+        for(int i=3 ; i<n ; i+=2){
+            if(isPrime(i)){
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    public boolean isPrime(int n){
+        for(int i=2 ; i*i<=n ; i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
 class Solution {
     public int countPrimes(int n) {
         if (n <= 2){
