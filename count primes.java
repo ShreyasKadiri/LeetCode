f15a6516344c1704cf0c6fb167d76fb7fdb535c4
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-10 17:15:19 +0530

    sieve

--- count primes.java
+++ count primes.java
@@ -1,29 +1,24 @@
 class Solution {
     public int countPrimes(int n) {
-        int count = 0;
-        if(n == 0 || n == 1){
-            return count;
+        if (n <= 2){
+            return 0;
+        }
+        boolean[] primes = new boolean[n];
+        for (int i=2; i<n; i++){
+            primes[i] = true;
         }
         
-        boolean[] primes = new boolean[n+1];
-        for (int i=2; i<=n; i++) {
-            primes[i] = true;
-         }
-        int sqrt = (int) Math.sqrt(n);
-        int num = 2;
-        while (num <= sqrt) {
-            if (primes[num]) {
-                int mul = num;
-                while (mul * num <= n) {
-                    primes[mul * num] = false;
-                    mul++;
+        for (int i=2; i<=Math.sqrt(n-1); i++) {
+            if (primes[i]) {
+                for (int j=2*i; j<n; j+=i){
+                    primes[j] = false;
                 }
             }
-            num++;
         }
-        for(int i=2; i<=n; i++){
-            if(primes[i] == true){
-                count+=1;
+        int count = 0;
+        for (int i=2; i<n; i++) {
+            if (primes[i]){
+                count++;
             }
         }
         return count;
