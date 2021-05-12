1590eed810ec783ef54b926ce5407bdaf2561cea
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-12 17:31:12 +0530

    dp

--- Range Sum Query 2D - Immutable.java
+++ Range Sum Query 2D - Immutable.java
@@ -15,7 +15,7 @@ class NumMatrix {
         }
    }
 
-    public int sumMatrix(int row1, int col1, int row2, int col2) {
+    public int sumRegion(int row1, int col1, int row2, int col2) {
         row1++;
         col1++;
         row2++;
@@ -29,4 +29,3 @@ class NumMatrix {
  * NumMatrix obj = new NumMatrix(matrix);
  * int param_1 = obj.sumRegion(row1,col1,row2,col2);
  */
-
