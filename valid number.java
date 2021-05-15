494c348b13ea5eadb24183c9a9ca64cfd43ad227
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-15 21:48:36 +0530

    exception handling + strings

--- valid number.java
+++ valid number.java
@@ -1,5 +1,17 @@
-class Solution {
+public class Solution {
     public boolean isNumber(String s) {
-        return s.trim().matches("[-+]?(\\d+\\.?|\\.\\d+)\\d*(e[-+]?\\d+)?");
+        try {
+            String number = s.trim();
+            if (number.length()==0 || (number.charAt(number.length()-1)!='.'
+                                && 
+                (number.charAt(number.length()-1)-'0'<0 || number.charAt(number.length()-1)-'0' >9 ))) {
+                return false;
+            }
+            double n = Double.parseDouble(number);
+            return true;
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
