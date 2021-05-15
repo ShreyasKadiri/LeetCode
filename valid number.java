13d835b55f20959f8380ac266cf6b09ae679e0d6
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-15 13:09:58 +0530

    regex

--- valid number.java
+++ valid number.java
@@ -1,27 +1,5 @@
-class Solution 
-{
-    public boolean isNumber(String s) 
-    {
-        s = s.trim();
-        boolean eSeen = false;
-        boolean numSeen = false;
-        boolean dotSeen = false;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (Character.isDigit(c)) {
-                numSeen = true;
-            } else if (c == 'e') {
-                if (eSeen || !numSeen) return false;
-                eSeen = true;
-                numSeen = false;
-            } else if (c == '.') {
-                if (eSeen || dotSeen) return false;
-                dotSeen = true;
-            } else if (c == '-' || c == '+') {
-                if (i != 0 && s.charAt(i - 1) != 'e') return false;
-            } else return false;
-        }
-        
-        return numSeen;
+class Solution {
+    public boolean isNumber(String s) {
+        return s.trim().matches("[-+]?(\\d+\\.?|\\.\\d+)\\d*(e[-+]?\\d+)?");
     }
 }
