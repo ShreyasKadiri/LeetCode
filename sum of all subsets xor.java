3d1e2b05830070996419ed8e70d6f2fc13938992
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-16 13:01:03 +0530

    bitwise

---
+++ sum of all subsets xor.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        int bits = 0;
+        for (int i=0; i<nums.length; i++){
+            bits |= nums[i];
+        }
+        int result = bits * (int)Math.pow(2, nums.length-1);
+        return result;
+    }
+}
