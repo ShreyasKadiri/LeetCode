2b1b7942205f2aa5d5130c80e359ab3b0f764347
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-18 18:06:12 +0530

    count number of opening parentheses

--- remove outer most parenthesis.java
+++ remove outer most parenthesis.java
@@ -1,13 +1,12 @@
 class Solution {
     public String removeOuterParentheses(String S) {
-        Stack<Character> stack = new Stack<Character>();
         StringBuilder sb = new StringBuilder("");
-        int opendParentheses = 0;
+        int openedParentheses = 0;
         for(char c : S.toCharArray()){
-            if(c == '(' && opendParenthesis++ > 0){
-                result.append(c);
-            }if(c == ')' && openedParenthesis-- > 1){
-                result.append(c);
+            if(c == '(' && openedParentheses++ > 0){
+                sb.append(c);
+            }if(c == ')' && openedParentheses-- > 1){
+                sb.append(c);
             }
         }
         return sb.toString();
