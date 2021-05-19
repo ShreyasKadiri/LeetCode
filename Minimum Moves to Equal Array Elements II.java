25ecbb061a4dd7e116b94fdcf69d91fed8b46c1d
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-19 12:45:56 +0530

    sorting

--- Minimum Moves to Equal Array Elements II.java
+++ Minimum Moves to Equal Array Elements II.java
@@ -10,3 +10,20 @@ class Solution {
         return minimumMoves;
     }
 }
+
+
+//Sorting
+class Solution {
+    public int minMoves2(int[] nums) {
+        int minimumMoves = 0;
+        Arrays.sort(nums);
+        int i=0;
+        int j = nums.length-1;
+        while(i < j){
+            minimumMoves+= nums[j] - nums[i];
+            i++;
+            j--;
+        }
+        return minimumMoves;
+    }
+}
