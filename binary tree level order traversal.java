7a2e5c8474dc3bd90b7bbb4fccb00d55cdbd4b28
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-20 17:36:18 +0530

    queue

--- binary tree level order traversal.java
+++ binary tree level order traversal.java
@@ -15,11 +15,11 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList>();
+        List<List<Integer>> result = new ArrayList<>();
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
         while(!queue.isEmpty()){
             int size = queue.size();
-            ArrayList<TreeNode> currentList = new ArrayList<TreeNode>();
+            List<Integer> currentList = new ArrayList<Integer>();
             for(int i=0; i<size; i++){
                 TreeNode currentNode = queue.poll();
                 if(currentNode.left!=null){
@@ -27,7 +27,7 @@ class Solution {
                 }if(currentNode.right!=null){
                     queue.add(currentNode.right);
                 }
-                currentList.add(queue.poll());
+                currentList.add(queue.poll().val);
             }
             result.add(currentList);
         }
