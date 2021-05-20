981bf95e5cb87e5a6f89753c7c405de8fa9d022a
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-20 18:00:07 +0530

    corner case added

--- binary tree level order traversal.java
+++ binary tree level order traversal.java
@@ -13,25 +13,29 @@
  *     }
  * }
  */
-class Solution {
+public class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         ArrayList<List<Integer>> result = new ArrayList<List<Integer>>();
+        if(root == null){
+            return result;
+        }
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
         queue.offer(root);
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             ArrayList<Integer> currentList = new ArrayList<Integer>();
             int size = queue.size();
-            for(int i=0; i<size; i++){
+            for (int i=0; i<size; i++){
                 TreeNode currentNode = queue.poll();
                 currentList.add(currentNode.val);
-                if(currentNode.left!=null){
-                    queue.add(currentNode.left);
-                }if(currentNode.right!=null){
-                    queue.add(currentNode.right);
+                if (currentNode.left!=null) {
+                    queue.offer(currentNode.left);
+                }
+                if (currentNode.right!=null) {
+                    queue.offer(currentNode.right);                    
                 }
             }
             result.add(currentList);
-        }
-        return result;
+        }       
+        return result;    
     }
 }
