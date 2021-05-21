cd1139d7114eef4e2ed22e84af2488421f91a84b
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-21 17:34:21 +0530

    flag

--- find and replace pattern.java
+++ find and replace pattern.java
@@ -1,20 +1,20 @@
 class Solution {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
        List<String> result  = new ArrayList<String>();
-        boolean flag = true;
         for(String word : words){
             int patternFrequency[] = new int[26];
             int currentString[] = new int[26];
+            boolean match = true;
             for(int i=0; i<word.length(); i++){
                 if(currentString[word.charAt(i) - 'a']!= patternFrequency[pattern.charAt(i) - 'a']){
-                    flag = false;
+                    match = false;
                     break;
                 }else {
                     currentString[word.charAt(i) - 'a'] = i+1;
                     patternFrequency[pattern.charAt(i) - 'a'] = i+1;
                 }
             }
-            if(flag == true){
+            if(match == true){
                 result.add(word);
             }
         }
