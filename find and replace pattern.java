b79071128c39692d1bbcce59c48843a4f3fc1237
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-21 17:29:52 +0530

    characters

--- find and replace pattern.java
+++ find and replace pattern.java
@@ -1,17 +1,17 @@
 class Solution {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
        List<String> result  = new ArrayList<String>();
-        boolean flag = false;
+        boolean flag = true;
         for(String word : words){
             int patternFrequency[] = new int[26];
             int currentString[] = new int[26];
             for(int i=0; i<word.length(); i++){
-                if(currentString[word.charAt(i) - 'o']!= patternFrequency[pattern.charAt(i) - 'o']){
+                if(currentString[word.charAt(i) - 'a']!= patternFrequency[pattern.charAt(i) - 'a']){
                     flag = false;
                     break;
                 }else {
-                    currentString[word.charAt(i) - 'o'] = i+1;
-                    patternFrequency[pattern.charAt(i) - 'o'] = i+1;
+                    currentString[word.charAt(i) - 'a'] = i+1;
+                    patternFrequency[pattern.charAt(i) - 'a'] = i+1;
                 }
             }
             if(flag == true){
