d7b63f1be9b3d0732170e40dc8a0622ce2c1e244
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-24 17:18:18 +0530

    ascii

--- to lowercase.java
+++ to lowercase.java
@@ -4,3 +4,18 @@ class Solution {
         return s.toLowerCase();
     }
 }
+ 
+//2nd approach using ASCII
+class Solution {
+    public String toLowerCase(String s) {
+        String lowerCase = "";
+        for(char c : s.toCharArray()){
+            if(c >='A' && c <= 'Z'){
+                lowerCase+=(char)( c + 32);
+            }else {
+                lowerCase+=c;
+            }
+        }
+        return lowerCase;
+    }
+}
