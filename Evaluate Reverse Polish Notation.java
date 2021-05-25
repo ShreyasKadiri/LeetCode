de6ee73b42b5e147f819a7633acf06a0f265c799
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-25 18:19:59 +0530

    stack and characters

--- Evaluate Reverse Polish Notation.java
+++ Evaluate Reverse Polish Notation.java
@@ -1,9 +1,8 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        int result = 0;
         Stack<Integer> stack = new Stack<Integer>();
         for(String token : tokens){
-            if(token == "+" || token == "-" || token == "*" || token == "/"){
+            if("+-*/".contains(token)){
                 stack.push(calculate(stack.pop(), stack.pop(), token));
             }else {
                 stack.push(Integer.parseInt(token));
@@ -13,15 +12,14 @@ class Solution {
     }
     
     public int calculate(int firstOperand, int secondOperand, String operator){
-        if(operator == "+"){
+        if("+".equals(operator)){
             return firstOperand + secondOperand;
-        }else if(operator == "*"){
+        }else if("*".equals(operator)){
             return firstOperand * secondOperand;
-        }else if(operator == "/"){
+        }else if("/".equals(operator)){
             return  secondOperand / firstOperand;
-        }else if(operator == "-"){
+        }else {
             return secondOperand - firstOperand;
         }
-        return -1;
     }
 }
