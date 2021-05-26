39f190f500178cd7de0e6cb5349ced49180ab78f
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-26 10:49:53 +0530

    sieve of erotosthenes

--- prime arrangements.java
+++ prime arrangements.java
@@ -3,16 +3,16 @@ class Solution{
         int MOD = (int) (1e9 + 7);
         int count = 0;
         boolean[] primes = new boolean[n+1]; 
-        Arrays.fill(prime, 2, n+1, true); 
+        Arrays.fill(primes, 2, n+1, true); 
         for(int i=2; i*i<=n; i++) {
             if (primes[i]) { 
-                for(int j=i*i; j<=n; j+=i) {\
+                for(int j=i*i; j<=n; j+=i) {
                     primes[j] = false;
                 }
             }
         }
         long result = 1;
-        for (boolean value : prime){
+        for (boolean value : primes){
             count += (value ? 1 : 0);
         }
         for (int i=2; i<=count; i++) {
