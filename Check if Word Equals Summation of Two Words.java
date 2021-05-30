712b2b06faf1de71a2fa80f2bbca686632626d74
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-30 17:14:55 +0530

    brute forcing

--- Check if Word Equals Summation of Two Words.java
+++ Check if Word Equals Summation of Two Words.java
@@ -16,10 +16,6 @@ class Solution {
             num3+=(c - 'a');
         }
         
-        int n1 = Integer.parseInt(num1);
-        int n2 = Integer.parseInt(num2);
-        int n3 = Integer.parseInt(num3);
-        
-        return n1 + n2 == n3;
+        return Integer.parseInt(num1) +  Integer.parseInt(num2) ==  Integer.parseInt(num3);
     }
 }
