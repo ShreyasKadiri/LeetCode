6599a895d9466e0f7f265ec29dd1bb750cbc5b31
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-31 16:54:27 +0530

    treemap

--- search suggestions system.java
+++ search suggestions system.java
@@ -1,28 +1,29 @@
 class Solution {
-    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-        List<List<String>> result = new List<>();
+    public List<List<String>> suggestedProducts(String[] products, String searchWord){
+        List<List<String>> result = new ArrayList<>();
         TreeMap<String, Integer> treeMap = new TreeMap<String, Integer>();
         Arrays.sort(products);
-        
+
         List<String> listOfProducts = Arrays.asList(products);
-        for(int i=0; i<listOfProducts.length(); i++){
-            map.put(listOfProducts, i);
+
+        for (int i=0; i<products.length; i++) {
+            treeMap.put(products[i], i);
         }
-        
+
         String prefix = "";
-        for(char c : searchWord.toCharArray()){
-            prefix+=c;
-            String high = map.ceilingKey(key);
-            String low = map.floorKey(key + "{");
-            
-            if(top == null || low == null){
+        for (char c : searchWord.toCharArray()) {
+            prefix += c;
+            String high = treeMap.ceilingKey(prefix);
+            String low = treeMap.floorKey(prefix + "{");
+            if (high == null || low == null) {
                 break;
             }
-            result.add(productsList.subList(map.get(ceiling), Math.min(map.get(ceiling)+3, map.get(floor)+1)));
+            result.add(listOfProducts.subList(treeMap.get(high), Math.min(treeMap.get(high) + 3, treeMap.get(low) + 1)));
         }
-         while (ans.size() < searchWord.length()){
-             ans.add(new ArrayList<>());
-         }
+
+        while (result.size() < searchWord.length()) {
+            result.add(new ArrayList<>());
+        }
         return result;
     }
 }
