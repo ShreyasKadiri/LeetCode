e02309221cf5c78c03939503eca4c425ad6cd16a
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-06-02 18:15:44 +0530

    brute force + dp

--- Interleave Strings.java
+++ Interleave Strings.java
@@ -18,3 +18,47 @@ class Solution {
         return flag1 || flag2;
     }
 }
+
+
+
+//DP 
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3){
+        int dp[][] = new int[s1.length()+1][s2.length()+1];
+        return solveStrings(s1,s2,s3,0,cache, 0, 0);
+    }
+    
+    public boolean solveStrings(String s1, String s2, String s3, int currentPosition, int dp[][], int i, int j){
+        if(currentPosition ==s3.length()){
+            if (i==s1.length() && j==s2.length()){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        if(i==s1.length() && j==s2.length() && currentPosition!=s3.length()){
+            return false;
+        }
+			
+        if (dp[i][j]!=0){
+            return dp[i][j] == 1 ? true : false;
+        }
+            
+			
+        boolean flag1 = false;
+        boolean flag2 = false;
+        
+        if(i<s1.length()){
+          if(s1.charAt(i) ==  s3.charAt(currentPosition)){
+              flag1 =  solveForStrings(i+1, j, currentPosition+1, s1, s2, s3, dp);
+          }
+        }
+        if (j<s2.length()){
+          if (s2.charAt(l2) == s3.charAt(currentPosition)){
+              flag2=  helper(l1, l2 +1 , position + 1, s1, s2, s3, dp);
+          }
+        }
+        dp[l1][l2] = (res1 || res2) == true ? 1 : -1;
+        return res1 || res2;
+    }
+}
