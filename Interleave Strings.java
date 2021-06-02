6ae7691a83903e09a3811a7b5de2c5cd5669e245
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-06-02 18:27:08 +0530

    brute force + dp updated

--- Interleave Strings.java
+++ Interleave Strings.java
@@ -1,64 +1,63 @@
 //Brute Force TC: o(2^n)
 class Solution {
-    public boolean isInterleave(String s1, String s2, String s3) {
-        if (s1.length() + s2.length() != s3.length()){
-            return false;
-        }
-        if (s1.length() == 0 && s2.length() == 0 && s3.length() == 0){
-            return true;
-        } 
-        boolean flag1 = false; 
-        if (s1.length() > 0 && (s1.charAt(0) == s3.charAt(0))){
-            flag1 = isInterleave(s1.substring(1), s2, s3.substring(1));
-        }
-        boolean flag2 = false; 
-        if (!flag1 && s2.length() > 0 && (s2.charAt(0) == s3.charAt(0))){
-            flag2 = isInterleave(s1, s2.substring(1), s3.substring(1));
-        }
-        return flag1 || flag2;
-    }
+	public boolean isInterleave(String s1, String s2, String s3) {
+		if (s1.length() + s2.length() != s3.length()) {
+			return false;
+		}
+		if (s1.length() == 0 && s2.length() == 0 && s3.length() == 0) {
+			return true;
+		}
+		boolean flag1 = false;
+		if (s1.length() > 0 && (s1.charAt(0) == s3.charAt(0))) {
+			flag1 = isInterleave(s1.substring(1), s2, s3.substring(1));
+		}
+		boolean flag2 = false;
+		if (!flag1 && s2.length() > 0 && (s2.charAt(0) == s3.charAt(0))) {
+			flag2 = isInterleave(s1, s2.substring(1), s3.substring(1));
+		}
+		return flag1 || flag2;
+	}
 }
 
 
 
 //DP 
 class Solution {
-    public boolean isInterleave(String s1, String s2, String s3){
-        int dp[][] = new int[s1.length()+1][s2.length()+1];
-        return solveStrings(s1, s2, s3, 0, dp, 0, 0);
-    }
-    
-    public boolean solveStrings(String s1, String s2, String s3, int currentPosition, int dp[][], int i, int j){
-        if(currentPosition ==s3.length()){
-            if (i==s1.length() && j==s2.length()){
-                return true;
-            }else{
-                return false;
-            }
-        }
-        if(i==s1.length() && j==s2.length() && currentPosition!=s3.length()){
-            return false;
-        }
-			
-        if (dp[i][j]!=0){
-            return dp[i][j] == 1 ? true : false;
-        }
-            
-			
-        boolean flag1 = false;
-        boolean flag2 = false;
-        
-        if(i<s1.length()){
-          if(s1.charAt(i) ==  s3.charAt(currentPosition)){
-              flag1 =  solveStrings(i+1, j, currentPosition+1, s1, s2, s3, dp);
-          }
-        }
-        if (j<s2.length()){
-          if (s2.charAt(l2) == s3.charAt(currentPosition)){
-              flag2=  solveStrings(i, j+1 , currentPosition+1, s1, s2, s3, dp);
-          }
-        }
-        dp[i][j] = (flag1 || flag2) == true ? 1 : -1;
-        return flag1 || flag2;
-    }
+	public boolean isInterleave(String s1, String s2, String s3) {
+		int dp[][] = new int[s1.length() + 1][s2.length() + 1];
+		return solveStrings(s1, s2, s3, 0, dp, 0, 0);
+	}
+
+	public boolean solveStrings(String s1, String s2, String s3, int currentPosition, int dp[][], int i, int j) {
+		if (currentPosition == s3.length()) {
+			if (i == s1.length() && j == s2.length()) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		if (i == s1.length() && j == s2.length() && currentPosition != s3.length()) {
+			return false;
+		}
+
+		if (dp[i][j] != 0) {
+			return dp[i][j] == 1 ? true : false;
+		}
+
+		boolean flag1 = false;
+		boolean flag2 = false;
+
+		if (i<s1.length()) {
+			if (s1.charAt(i) == s3.charAt(currentPosition)) {
+				flag1 = solveStrings(s1, s2, s3, currentPosition + 1, dp, i + 1, j);
+			}
+		}
+		if (j<s2.length()) {
+			if (s2.charAt(j) == s3.charAt(currentPosition)) {
+				flag2 = solveStrings(s1, s2, s3, currentPosition + 1, dp, i, j + 1);
+			}
+		}
+		dp[i][j] = (flag1 || flag2) == true ? 1 : -1;
+		return flag1 || flag2;
+	}
 }
