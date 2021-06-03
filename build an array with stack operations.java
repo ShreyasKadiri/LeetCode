be16587ffabbb934d27b1988b055f094d82f69f7
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-06-03 17:56:52 +0530

    size of list optimised with numoperations

--- build an array with stack operations.java
+++ build an array with stack operations.java
@@ -1,5 +1,7 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
+        int numberOperations = target.length;
+        numberOperations+= ((target[target.length-1] -target.length)*2);
         List<String> operations = new ArrayList<String>();
         int input = 1;
         for(int i=0; i<target.length; i++){
