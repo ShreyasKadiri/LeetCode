9455a6991d438c978fc390a72a8bd5ecf13d33d8
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-06-04 19:08:55 +0530

    based on prefix sum

--- Minimum Value to Get Positive Step by Step Sum.java
+++ Minimum Value to Get Positive Step by Step Sum.java
@@ -1,10 +1,16 @@
-class Solution{
-public int minStartValue(int[] nums) {
-    int sum = 0, min_sum = 0;
-    for (var n : nums) {
-        sum += n;
-        min_sum = Math.min(min_sum, sum);
+public class Solution {
+    public int minStartValue(int[] nums) {
+        int[] prefixSum = new int[nums.length]; 
+        prefixSum[0] = nums[0];
+        int minimumPrefixSum = prefixSum[0];
+        for(int i=1; i<nums.length; i++){
+            prefixSum[i] = prefixSum[i-1] + nums[i];
+            minimumPrefixSum = Math.min(minimumPrefixSum, prefixSum[i]);
+        }
+        if(minimumPrefixSum > 0){
+            return 1;
+        }else{
+            return 1 + -1*minimumPrefixSum;
+        }
     }
-    return 1 - min_sum;
-  }
 }
