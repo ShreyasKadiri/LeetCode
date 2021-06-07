45844aed564f5848af0cb33d19d23b623c26c816
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-06-07 16:58:28 +0530

    recursion + greedy

--- Min Cost Climbing Stairs.java
+++ Min Cost Climbing Stairs.java
@@ -1,4 +1,21 @@
-//DP
+//Recursion
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        return findMinimumCost(cost.length, cost);
+    }
+
+    private int findMinimumCost(int i, int[] cost) {
+        if (i == 0 || i == 1) {
+            return 0;
+        }
+        int first = findMinimumCost(i-2, cost) + cost[i-2];
+        int second = findMinimumCost(i-1, cost) + cost[i-1];
+        return Math.min(first, second);
+    }
+}
+
+
+//Greedy
 class Solution {
     public int minCostClimbingStairs(int[] cost){
         for (int i=2; i<cost.length; i++) {
