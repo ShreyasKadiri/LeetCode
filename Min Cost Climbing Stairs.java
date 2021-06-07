4d8229cea5bd6df6cca148b227d3e08eafb0b3cf
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-06-07 16:48:44 +0530

    dp

--- Min Cost Climbing Stairs.java
+++ Min Cost Climbing Stairs.java
@@ -1,9 +1,9 @@
+//DP
 class Solution {
-    public int minCostClimbingStairs(int[] cost) {
-        int price=0;
-        for(int i=0; i<cost.length-2; i++){
-            price+=Math.min(cost[i+1], cost[i+2]);
+    public int minCostClimbingStairs(int[] cost){
+        for (int i=2; i<cost.length; i++) {
+            cost[i] += Math.min(cost[i-1], cost[i-2]);
         }
-        return price;
+        return Math.min(cost[cost.length-1], cost[cost.length-2]);
     }
 }
