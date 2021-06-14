c98ca086e86ba1ad2534b49596cf2696844f931b
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-06-14 16:39:12 +0530

    greedy with sorting

--- Maximum Units on a Truck.java
+++ Maximum Units on a Truck.java
@@ -1,10 +1,15 @@
 class Solution {
     public int maximumUnits(int[][] boxTypes, int truckSize) {
+        Arrays.sort(boxTypes, (a, b) -> b[1] - a[1]);
         int result = 0;
-        Arrays.sort(boxTypes, (a,b) -> (b[0] - a[0]));
-        for(int boxType[] : boxTypes){
-            if(result <= truckSize){
-                result+= boxType[1];
+        for (int i=0; i<boxTypes.length && truckSize > 0; i++) {
+            int currentBoxType = boxTypes[i][0];
+            int unitsCount = boxTypes[i][1];
+            truckSize = truckSize - currentBoxType;
+            result+= currentBoxType * unitsCount;   //Total capacity
+            if (truckSize < 0) {
+                result += truckSize * unitsCount;
+                break;
             }
         }
         return result;
