459b73286fc74ca09f3a75cd96646cb86153ed18
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-06-14 16:56:18 +0530

    greedy with bucketing

--- Maximum Units on a Truck.java
+++ Maximum Units on a Truck.java
@@ -15,3 +15,27 @@ class Solution {
         return result;
     }
 }
+
+
+//Bucketing
+class Solution {
+    public int maximumUnits(int[][] boxTypes, int truckSize){
+        int [] unitCount = new int[1001];
+        int count = 0;
+        for (int [] boxType : boxTypes){
+            unitCount[boxType[1]] += boxType[0];
+        }
+        for (int i=unitCount.length-1; i>=0; i--){
+            if (unitCount[i] == 0){
+                continue;
+            }
+            int num = Math.min(unitCount[i], truckSize);
+            count += num * i;
+            truckSize -= num;
+            if (truckSize == 0){
+                break;
+            }
+        }
+        return count;
+    }
+}
