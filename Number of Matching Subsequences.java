4ec70ebd3a8ab46bb59e16254a90b3463e53ef04
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-06-22 17:26:04 +0530

    queue and hashmap

--- Number of Matching Subsequences.java
+++ Number of Matching Subsequences.java
@@ -1,27 +1,58 @@
+//Brute Force
 class Solution {
-    public int numMatchingSubseq(String s, String[] words) {
-        int count = 0;
-        int wordFrequency[] = new int[26];
-        int stringFrequency[] = new int[26];
-        int length = 0;
-        
-        for(char c : s.toCharArray()){
-            stringFrequency[c - 'a']++;
-        }
-        
-        for(String word : words){
-            for(char c : word.toCharArray()){
-                wordFrequency[c - 'a']++;
+    public int numMatchingSubseq(String S, String[] words) {
+        int result=0;
+        for (String word : words) {
+            if (isSubsequence(word,S)) {
+                result++;
             }
-            
-            for(char c : word.toCharArray()){
-                if(wordFrequency[c - 'a'] != stringFrequency[c - 'a']){
-                    break;
-                }else {
-                    count++;
-                }
-            }   
         }
-        return count;
+        return result;
+    }
+
+    public boolean isSubsequence(String t, String s) {
+        int j=0;
+        for (int i=0; i<s.length() && j<t.length(); i++) {
+            if (s.charAt(i) == t.charAt(j)) {
+                j++;
+            }
+        }
+        return j==t.length();
+    }
+}
+
+
+// Queue and map  TC: O(nL) and SC: O(n)
+class Solution {
+    public int numMatchingSubseq(String S, String[] words) {
+        int result=0;
+        HashMap<Character,Queue<String>> map = new HashMap<Character,Queue<String>>();
+        for (char c:S.toCharArray()) {
+            if (!map.containsKey(c)) {
+                map.put(c, new LinkedList<>());
+            }
+        }
+        for(String word : words) {
+            if(word.length()>S.length())
+                continue;
+            if (map.containsKey(word.charAt(0))) {
+                map.get(word.charAt(0)).add(word);
+            }
+        }
+
+        for (char c:S.toCharArray()) {
+            Queue<String> queue = map.get(c);
+            int size=queue.size();
+            for (int i=0; i<size; i++) {
+                String curr=queue.remove();
+                if (curr.length()==1) {
+                    result++;
+                }
+                else if (map.containsKey(curr.charAt(1))) {
+                    map.get(curr.charAt(1)).add(curr.substring(1));
+                }
+            }
+        }
+        return result;
     }
 }
