45b8d8fb273b3c2df15b2048905823ddfbdd82bf
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-06-23 17:50:34 +0530

    extra nodes

---
+++ Reverse Linked List II.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode fakeHead = new ListNode(-1);
+        fakeHead.next = head;
+        ListNode prev = fakeHead;
+        ListNode curr = fakeHead.next;
+        int i = 1;
+        while (i < m) {
+            prev = curr;
+            curr = curr.next;
+            i++;
+        }
+        ListNode node = prev;
+        while (i <= n) {
+            ListNode tmp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = tmp;
+            i++;
+        }
+        node.next.next = curr;
+        node.next = prev;
+        return fakeHead.next;
+    }
+}
