06f4031eefb193d38f33bd85bcbb881d05f22ddf
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-06-26 12:11:40 +0530

    without prefixsum array

--- Range Sum Query - Immutable.java
+++ Range Sum Query - Immutable.java
@@ -20,3 +20,28 @@ class NumArray {
  * NumArray obj = new NumArray(nums);
  * int param_1 = obj.sumRange(left,right);
  */
+
+
+//without prefixsum array
+class NumArray {
+    int[] nums;
+    public NumArray(int[] nums) {
+        for(int i = 1; i < nums.length; i++){
+            nums[i] += nums[i - 1];
+        }
+        this.nums = nums;
+    }
+    
+    public int sumRange(int i, int j) {
+        if(i == 0){
+            return nums[j];
+        }
+        return nums[j] - nums[i - 1];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
