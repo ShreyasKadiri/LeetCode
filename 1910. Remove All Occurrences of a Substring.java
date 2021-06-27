fc5d93c5b3b3f3318744ca628716c1b820496cd4
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-06-27 13:44:00 +0530

    3 approches

--- 1910. Remove All Occurrences of a Substring.java
+++ 1910. Remove All Occurrences of a Substring.java
@@ -3,3 +3,55 @@ class Solution {
         return s.contains(part) ? removeOccurrences(s.replaceAll(part, ""), part) : s;
     }
 }
+
+
+
+
+class Solution {
+    public String removeOccurrences(String s, String part) {
+	        StringBuilder sb = new StringBuilder(s);
+	        while(true){
+	            if(sb.indexOf(part)<0){
+                    break;
+                }
+	            int index = sb.indexOf(part);
+	            sb.delete(index, index+part.length());
+	        }
+	        return sb.toString();
+    }
+}
+
+
+
+class Solution {
+  public String removeOccurrences(String s, String part) {
+    Stack < Character > stack = new Stack < Character > ();
+    for (int i = 0; i < s.length(); i++) {
+      stack.add(s.charAt(i));
+      if (stack.size() >= part.length()) {
+        String l = "";
+        for (int j = part.length() - 1; j >= 0; j--) {
+          if (part.charAt(j) != stack.peek()) {
+            int f = 0;
+            while (f != l.length()) {
+              stack.add(l.charAt(f));
+              f++;
+            }
+            break;
+          } else {
+            l = stack.peek() + l;
+            stack.pop();
+          }
+        }
+      }
+    }
+
+    StringBuilder sb = new StringBuilder();
+    while (!stack.isEmpty()) {
+      sb.append(stack.peek());
+      stack.pop();
+    }
+    sb.reverse();
+    return sb.toString();
+  }
+}
