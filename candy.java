8c8f253a41cefdce9b1b3c26256fe54e016dac6f
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-06-27 15:12:34 +0530

    array + linear traversal

--- candy.java
+++ candy.java
@@ -1,43 +1,25 @@
-class Solution {
+class Solution{
     public int candy(int[] ratings) {
-        if(ratings.length==0 || ratings==null){
-            return -1;
-            
-        if(ratings.length<=1){
-            ratings.length;
+        int sum=0;
+        int[] candies = new int[ratings.length];
+        for(int i=0; i<candies.length; i++){
+            candies[i] = 1;
         }
-
-            int candiesCount[] = new int[ratings.length];
-            Arrays.fill(candiesCount,1);
-            int i;
-            int j;
-            
-    // Traversing from right to left:
-         i=rating.length-2;
-        while(i>=0){
-            j=i+1;
-        if(ratings[i] > ratings[j] && candiesCount[i]<= candiesCount[j])
-            candiesCount[i] = candiesCount[j] + 1;
-        else if(ratings[i] < ratings[j] && candiesCount[j] <= candiesCount[i])
-            candiesCount[j] = candiesCount[i] + 1;
-        i--;
+        for(int i=0; i<ratings.length-1; i++){
+            if(ratings[i+1] > ratings[i]){
+                candies[i+1] = candies[i] + 1;
+            }
+        }
+        for(int i=ratings.length-1; i>0; i--){
+            if(ratings[i-1]>ratings[i]){
+                if(candies[i-1] < candies[i]+1){
+                    candies[i-1] = candies[i] + 1;
+                }
+            }
+        }
+        for(int i=0; i<candies.length; i++){
+            sum+= candies[i];
+        }
+        return sum;
     }
-    
-    // Traveling from left to right:
-     i=1;
-     j=0;
-    while(i<n){
-        j=i-1;
-        if(ratings[i] > ratings[j] && candiesCount[i] <= candiesCount[j])
-            candiesCount[i] = candiesCount[j] + 1;
-        else if(ratings[i] < ratings[j] && candiesCount[j] <= candiesCount[i])
-            candiesCount[j] = candiesCount[i] + 1;
-        i++;
-    }
-    int sum=0;
-    for(int i=0; i<ratings.length; i++){
-        sum+=candy[i];
-    }
-    return sum;
-  }      
 }
