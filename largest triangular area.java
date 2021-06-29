e9602129c82b437ce1d5c3be41ddf71e49b3f069
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-06-29 17:56:39 +0530

    math

--- largest triangular area.java
+++ largest triangular area.java
@@ -2,8 +2,8 @@ class Solution {
     public double largestTriangleArea(int[][] points) {
         double result = 0;
         for (int[] point : points)
-            for (int[] values : point)
-                for (int[] value: point)
+            for (int[] values : points)
+                for (int[] value: points)
                     result = Math.max(result, 0.5 * Math.abs(point[0] * values[1] + values[0] * value[1] + value[0] * point[1]- values[0] * point[1] - value[0] * values[1] - point[0] * value[1]));
         return result;
     }
