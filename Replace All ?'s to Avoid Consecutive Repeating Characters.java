b5e01c61e29f9d95535ee7f9837c5d2b0fc416dc
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-06-30 17:59:36 +0530

    stringbuilder

--- Replace All ?'s to Avoid Consecutive Repeating Characters.java
+++ Replace All ?'s to Avoid Consecutive Repeating Characters.java
@@ -1,20 +1,38 @@
 class Solution {
     public String modifyString(String s) {
-        String result = "";
-        for (int i=0; i<s.length(); i++) {
-            if (s.charAt(i) == '?'){
-                for (char c='a'; c<= 'c'; c++){
-                    if (i>0 && s.charAt(i-1) == c){
-                        continue;
+       if(s == null || s.length() <= 0){
+            return s;
+        }
+        if(s.length() == 1 && s.charAt(0) == '?'){
+            return "a";
+        }
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while(i < s.length()){
+            if(s.charAt(i) != '?'){
+                sb.append(s.charAt(i));
+            }else{
+                for(char c = 'a'; c <= 'z'; c++){
+                    if(i == 0){
+                        if(c != s.charAt(i + 1)){
+                            sb.append(c);
+                            break;
+                        }
+                    }else if(i == s.length() - 1){
+                        if(c != sb.charAt(sb.length() - 1)){
+                            sb.append(c);
+                            break;
+                        }
+                    }else{
+                        if(c != sb.charAt(sb.length() - 1) && c != s.charAt(i + 1)){
+                            sb.append(c);
+                            break;
+                        }
                     }
-                    if (i+1 < s.length() && s.charAt(i+1) == c){
-                        continue;
-                    }
-                    result+= c;
-                    break;
                 }
             }
+            i++;
         }
-        return result;
+        return sb.toString();
     }
 }
