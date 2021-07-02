e5872610d5a8041b1516b3ae8d0309fd4ddf52b3
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-07-02 16:57:23 +0530

    priorityqueue and binary search

--- Find K Closest Elements.java
+++ Find K Closest Elements.java
@@ -15,3 +15,25 @@ class Solution {
         return result;
     }
 }
+
+
+//Binary search
+class Solution {
+    public List<Integer> findClosestElements(int[] nums, int k, int x) {
+        List<Integer> result = new ArrayList<Integer>();
+        int low = 0;
+        int high = nums.length - k;
+        while (low < high){
+            int mid = low + (high-low)/2;
+            if (x - nums[mid] > nums[mid+k] - x) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        for (int i=low; i<low+k; i++) {
+            result.add(nums[i]);
+        }
+        return result;
+    }
+}
