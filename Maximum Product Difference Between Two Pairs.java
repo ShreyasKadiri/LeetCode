a0b09e3ab4f05ba0d784e2274746b5941f7797b5
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-07-03 12:52:26 +0530

    o(nlogn) --> o(n)

--- Maximum Product Difference Between Two Pairs.java
+++ Maximum Product Difference Between Two Pairs.java
@@ -1,6 +1,49 @@
+//o(nlogn)
 class Solution {
     public int maxProductDifference(int[] nums) {
         Arrays.sort(nums);
         return nums[nums.length-1]*nums[nums.length-2] - nums[0]*nums[1];
+    }
+}
+
+//o(n)
+class Solution {
+    int firstMinimum = Integer.MAX_VALUE;
+    int secondMinimum = Integer.MAX_VALUE;
+    int firstLargest = Integer.MIN_VALUE;
+    int secondLargest = Integer.MIN_VALUE;
+    
+    int small[] = new int[2];
+    int large[] = new int[2];
+        
+    public int maxProductDifference(int[] nums) {
+       small = smallestNumbers(nums);
+       large =largestNumbers(nums);
+        return large[0]*large[1] - small[1]*small[0];
+    }
+    
+    
+    public int[] smallestNumbers(int nums[]){
+        for (int i=0; i<nums.length; i++) {
+            if(firstMinimum>nums[i]){
+                secondMinimum = firstMinimum;
+                firstMinimum = nums[i];
+            }else if(secondMinimum>nums[i]){
+                secondMinimum = nums[i];
+            }
+        }
+        return new int[]{firstMinimum,secondMinimum};
+    }
+    
+    public int[] largestNumbers (int nums[]){
+        for (int i=0; i<nums.length; i++) {
+            if(firstLargest < nums[i]){
+                secondLargest = firstLargest;
+                firstLargest = nums[i];
+            }else if(secondLargest < nums[i]){
+                secondLargest = nums[i];
+            }
+        }
+        return new int[]{firstLargest, secondLargest};
     }
 }
