55bb15f91ee4771e7255e90d095759f8c63c27a4
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-07-03 12:55:49 +0530

    single pass

--- Maximum Product Difference Between Two Pairs.java
+++ Maximum Product Difference Between Two Pairs.java
@@ -21,8 +21,7 @@ class Solution {
        large =largestNumbers(nums);
         return large[0]*large[1] - small[1]*small[0];
     }
-    
-    
+ 
     public int[] smallestNumbers(int nums[]){
         for (int i=0; i<nums.length; i++) {
             if(firstMinimum>nums[i]){
@@ -45,5 +44,28 @@ class Solution {
             }
         }
         return new int[]{firstLargest, secondLargest};
+    }
+}
+
+
+//o(n) Single Pass
+class Solution{
+    public int maxProductDifference(int[] nums) {
+	int largest = 0, secondLargest = 0, smallest = Integer.MAX_VALUE, secondSmallest = Integer.MAX_VALUE;
+	for(int i=0;i<nums.length;i++) {
+        if(nums[i]>=largest) {
+            secondLargest = largest;
+            largest = nums[i];
+		} else if(nums[i]>secondLargest){
+            secondLargest = nums[i];
+        }
+        if(nums[i]<=smallest) {
+            secondSmallest = smallest;
+			smallest = nums[i];
+		} else if(nums[i]<secondSmallest){
+            secondSmallest = nums[i];
+        }
+	}
+	return largest * secondLargest - smallest * secondSmallest;
     }
 }
