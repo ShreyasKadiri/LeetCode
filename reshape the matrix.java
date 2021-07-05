f4a64622ace2147fffa2459a52b639ebc31d223d
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-07-05 18:58:23 +0530

    matrix

--- reshape the matrix.java
+++ reshape the matrix.java
@@ -1,22 +1,22 @@
 class Solution {
-    public int[][] matrixReshape(int[][] mat, int r, int c) {
-        if (mat.length * mat[0].length!= r*c)  {
+    public int[][] matrixReshape(int[][] mat, int r, int c){
+        if(r*c == (mat.length * mat[0].length)){
+            int[][] result = new int[r][c];
+            int startingRow = 0;
+            int startingColumn = 0;
+            for(int i=0; i<r; i++){
+                for(int j=0; j<c; j++){
+                    result[i][j] = mat[startingRow][startingColumn];
+                    startingColumn++;
+                    if(startingColumn == mat[0].length){
+                        startingRow++;
+                        startingColumn = 0;
+                    }
+                }
+            }
+            return result;
+        }else{
             return mat;
         }
-        int[][] result = new int[r][c];
-        int startingRow = 0;
-        int startingColumn = 0;
-        
-        for(int i=0; i<mat.length; i++){
-            for(int j=0; j<mat[0].length; j++){
-                if(startingColumn == c){
-                    startingRow++;
-                    startingRow = 0;
-                }
-                result[startingRow][startingColumn] = mat[i][j];
-                startingColumn++;
-            }
-        }
-        return result;
     }
 }
