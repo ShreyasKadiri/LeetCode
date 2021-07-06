c1ac53bd4b9aacb412b7b92aa7335c91d4ff640e
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-07-06 18:16:46 +0530

    math

--- Smallest Range I.java
+++ Smallest Range I.java
@@ -1,5 +1,15 @@
 class Solution {
     public int smallestRangeI(int[] A, int K) {
-        return (A[A.length-1] - K) - (A[0]+K); 
+        int minimumValue = A[0];
+        int maximumValue = A[0];
+        for(int i=0; i<A.length; i++){
+            minimumValue = Math.min(minimumValue, A[i]);
+            maximumValue = Math.max(maximumValue, A[i]);
+        }
+        if(minimumValue + K >= maximumValue - K){
+            return 0;
+        }else {
+            return (maximumValue - K) - (minimumValue + K); 
+        }
     }
 }
