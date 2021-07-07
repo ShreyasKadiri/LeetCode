a5ede13b7fa10671991c1ad703a7bb5fb6bcd84a
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-07-07 17:54:33 +0530

    sorting + priority queue

--- Kth Smallest Element in a Sorted Matrix.java
+++ Kth Smallest Element in a Sorted Matrix.java
@@ -13,3 +13,20 @@ class Solution {
         return matrixElements[k-1];
     }
 }
+
+
+//PriorityQueue
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<Integer>((a,b) -> (b-a));
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                heap.add(matrix[i][j]);
+            }
+        }
+        while(heap.size() > k){
+            heap.poll();
+        }        
+        return heap.poll();
+    }
+}
