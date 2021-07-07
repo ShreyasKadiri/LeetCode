1028878aad3e6f3bb2912ddf01a635dba9b06108
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-07-07 18:32:32 +0530

    sorting + heap + binary search

--- Kth Smallest Element in a Sorted Matrix.java
+++ Kth Smallest Element in a Sorted Matrix.java
@@ -30,3 +30,36 @@ class Solution {
         return heap.poll();
     }
 }
+
+//Binary Search
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int low = matrix[0][0];
+        int high = matrix[matrix.length-1][matrix.length-1];
+        while(low < high){
+            int mid = low + (high - low)/2;
+            int placement = findSmallerElementsThanMid(matrix, mid);
+            if(placement < k){
+                low = mid + 1;
+            }else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+    
+    public int findSmallerElementsThanMid(int matrix[][], int mid){
+        int count = 0;
+        int i = matrix.length-1;
+        int j = 0;
+        while(i>=0 && j<matrix.length){
+            if(matrix[i][j] > mid){
+                i--;
+            }else {
+                count = count + i + 1;
+                j++;
+            }
+        }
+        return count;
+    }
+}
