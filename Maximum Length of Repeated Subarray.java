d7053e07b67cf63bdba2a8b24c9591b28b6f4517
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-07-08 18:29:33 +0530

    recursion + dp

--- Maximum Length of Repeated Subarray.java
+++ Maximum Length of Repeated Subarray.java
@@ -1,3 +1,24 @@
+//Recursion
+class Solution {
+    public int findLength(int[] nums1, int[] nums2) {
+        int result = 0;
+        for(int i=0; i<nums1.length; i++){
+            for(int j=0; j<nums2.length; j++){
+                 result = Math.max(result, solveRecursively(nums1, nums2, i, j));
+            } 
+        } 
+        return result;
+    }
+    
+    private int solveRecursively(int nums1[], int nums2[], int i, int j) {
+        if(i == nums1.length || j == nums2.length || nums1[i] != nums2[j]){
+            return 0;
+        }
+        return 1 + solveRecursively(nums1, nums2, i+1, j+1);
+    }
+}
+
+//DP
 class Solution {
     public int findLength(int[] nums1, int[] nums2) {
         int dp[][] = new int[nums1.length+1][nums2.length+1];
