582df4ff0bdd430b067c8031eec8c3394889f965
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-07-09 17:21:19 +0530

    hashmap + one liner

--- Find Center of Star Graph.java
+++ Find Center of Star Graph.java
@@ -14,3 +14,11 @@ class Solution {
         return -1;
     }
 }
+
+
+//One Liner
+class Solution {
+    public int findCenter(int[][] edges) {
+        return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
+    }
+}
