c3239336342db0eb9930f88ec7b8201e923b8efb
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-07-10 13:21:59 +0530

    binary search

--- find kth smallest pair distance.java
+++ find kth smallest pair distance.java
@@ -22,3 +22,33 @@ class Solution {
         return 0;
     }
 }
+
+//Binary Search
+class Solution {
+    public int smallestDistancePair(int[] nums, int k) {
+        Arrays.sort(nums);
+        int low = 0;
+        int high = nums[nums.length-1] - nums[0];
+        while(low < high){
+            int mid = low + (high - low)/2;
+            if(issmallpairs (nums, k, mid)){
+                high = mid;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+    
+    private boolean issmallpairs(int[] nums, int k, int mid){
+        int count = 0, low = 0;
+        for(int i=1; i<nums.length; i++){
+            while(nums[i] - nums[low] > mid){
+                low++;
+            }
+            count+= i - low;
+        }
+        return (count>=k);
+    }
+}
