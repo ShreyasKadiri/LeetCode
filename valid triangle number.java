cd3ea994f172d2b4d3236ed1c05cb380888d7e20
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-07-15 18:13:37 +0530

    bf and sorting

--- valid triangle number.java
+++ valid triangle number.java
@@ -13,3 +13,28 @@ class Solution {
         return count;
     }
 }
+
+
+//O(n2)
+class Solution {
+    public int triangleNumber(int[] nums) {
+        int result  = 0;
+        if (nums.length < 3){
+            return result;
+        }
+        Arrays.sort(nums);
+        for (int i=2; i<nums.length; i++) {
+            int left = 0;
+            int right = i-1;
+            while (left < right) {
+                if (nums[left] + nums[right] > nums[i]) {
+                    result+= (right - left);
+                    right--;
+                }else {
+                    left++;
+                }
+            }
+        }
+        return result;
+    }
+}
