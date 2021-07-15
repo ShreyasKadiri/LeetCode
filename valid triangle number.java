579ab7df5de055426916a5e874e206f5e68943c9
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-07-15 17:57:11 +0530

    bf

---
+++ valid triangle number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int triangleNumber(int[] nums) {
+        int count = 0;
+        for(int i=0; i<nums.length; i++){
+            for(int  j=i+1; j<nums.length; j++){
+                for(int k=j+1; k<nums.length; k++){
+                    if(nums[i]+nums[j]>nums[k] && nums[i]+nums[k]>nums[j] && nums[j]+nums[k]>nums[i] ){
+                        count+=1;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
