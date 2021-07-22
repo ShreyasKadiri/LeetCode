a89de06c67c655d87d49bf03eba23975eb735eba
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-07-22 18:04:03 +0530

    two arrays

---
+++ Partition Array into Disjoint Intervals.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int partitionDisjoint(int[] nums) {
+        int maximumOnLeft[] = new int[nums.length];
+        int minimumOnRight[] = new int[nums.length];
+        int maximum = Integer.MIN_VALUE;
+        int minimum = Integer.MAX_VALUE;
+        
+        //Getting maximum element on the left side
+        for(int i=0; i<nums.length; i++){
+            maximum = Math.max(maximum, nums[i]);
+            maximumOnLeft[i] = maximum;
+        }
+        
+        
+        //Getting minimum element on right side
+         for(int i=nums.length-1; i>=0; i--){
+            minimum = Math.min(minimum, nums[i]);
+            minimumOnRight[i] = minimum;
+        }
+        
+        for(int i=1; i<nums.length; i++){
+            if(maximumOnLeft[i-1] <= minimumOnRight[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
