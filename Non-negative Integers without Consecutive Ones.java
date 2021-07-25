d1bccf73461fa8fb6d89329e045771d0d72d8a6f
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-07-25 13:59:53 +0530

    cache

--- Non-negative Integers without Consecutive Ones.java
+++ Non-negative Integers without Consecutive Ones.java
@@ -1,3 +1,4 @@
+//Brute Force  --- TLE
 class Solution {
     public int findIntegers(int n) {
         int count = 0;
@@ -17,5 +18,31 @@ class Solution {
             }
         }     
         return false;
+    }
+}
+
+
+class Solution {
+    public int findIntegers(int n) {
+        if (n < 2) {
+            return n + 1;
+        }
+        StringBuilder sb = new StringBuilder(Integer.toBinaryString(n)).reverse();
+        int[] cache = new int[sb.length()];
+        cache[0] = 1;
+        cache[1] = 2;
+        for (int i=2; i<sb.length(); i++){
+            cache[i] = cache[i-1] + cache[i-2];
+        }
+        int result = 0;
+        for (int i=sb.length()-1; i>=0; i--) {
+            if (sb.charAt(i) == '1'){
+                result+= cache[i];
+                if (i<sb.length()-1 && sb.charAt(i+1) == '1') { 
+                    return result;
+                }
+            }
+        }
+        return result+1;
     }
 }
