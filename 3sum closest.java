7deea65e3130db8edfe4a67fc79b177f38899b5d
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-07-27 16:40:22 +0530

    o(n2) with sorting

--- 3sum closest.java
+++ 3sum closest.java
@@ -13,3 +13,28 @@ class Solution {
         return closestSum;
     }
 }
+
+
+//n2
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        int result = nums[0] + nums[1] + nums[nums.length-1];
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length-2; i++){
+            int low = i+1;
+            int high = nums.length-1;
+            while(low < high){
+                int currentSum = nums[i] + nums[low] + nums[high];
+                if(currentSum <= target){
+                    low++;
+                }else {
+                    high--;
+                }
+                if(Math.abs(currentSum - target) < Math.abs(result - target)){
+                    result= currentSum;
+                }
+            }
+        }
+        return result;
+    }
+}
