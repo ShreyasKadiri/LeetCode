6b7b4582a929b086d94efc177d0f355e6906d8c8
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-10-04 19:49:33 +0530

    bfs

--- island perimeter.java
+++ island perimeter.java
@@ -17,3 +17,31 @@ class Solution {
         return count;
     }
 }
+
+
+//Solution 2
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int count=0;
+        boolean visited[][] = new boolean[grid.length][grid[0].length];
+        for(int  i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                if(grid[i][j] == 1){
+                   return bfs(grid, i, j, visited);
+                }
+            }
+        }
+        return 0;
+    }
+    
+    private int bfs(int grid[][], int i, int j, boolean visited[][]){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j] == 0){
+            return 1;
+        }
+        if(visited[i][j]){
+            return 0;
+        }
+        visited[i][j] = true;
+        return bfs(grid, i-1, j, visited) + bfs(grid, i+1, j, visited) + bfs(grid, i, j-1, visited) + bfs(grid, i, j+1, visited);
+    }
+}
