16ac4ae0c9e189689be9b2608f5d4e027aa058c5
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-10-24 14:48:04 +0530

    recursion

---
+++ Count Complete Tree Nodes.java
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if(root==null){
+             return 0;
+        }
+        return countNodes(root.left) + 1 +countNodes(root.right);
+    }
+}
