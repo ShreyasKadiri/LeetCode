96bbacaabbc6900c7d4ec96a130b278aaa4d1cf7
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-11-11 13:16:37 +0530

    steps added

--- Minimum Value to Get Positive Step by Step Sum.java
+++ Minimum Value to Get Positive Step by Step Sum.java
@@ -1,5 +1,12 @@
 public class Solution {
     public int minStartValue(int[] nums) {
+          /*
+        Step 1: Take prefix Sum
+        Step 2: Get the minimum prefix Sum out of all
+        Step 3: Find a number that needs to be added to this minimum prefix Sum from Step 2 to make it > 0
+        Ex: Let minimum prefix sum be -4, add 5 to -4 which gives 1, i.e abs(minimumPrefixSum) + 1
+        */
+        
         int[] prefixSum = new int[nums.length]; 
         prefixSum[0] = nums[0];
         int minimumPrefixSum = prefixSum[0];
