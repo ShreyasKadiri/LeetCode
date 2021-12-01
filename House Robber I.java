f6080b915fdcc7a6a52069a258bbf18de7d095ad
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-12-01 11:57:42 +0530

    dp

---
+++ House Robber I.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(int[] nums) {
+        int inclusive = nums[0];
+        int exclusive = 0;
+        int exclusiveNew;
+        for (int i=1; i<nums.length; i++){
+            exclusiveNew = (inclusive > exclusive) ? inclusive : exclusive;
+            inclusive = exclusive + nums[i];
+            exclusive = exclusiveNew;
+        }
+        return ((inclusive > exclusive) ? inclusive : exclusive);
+    }
+}
