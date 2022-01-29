fa07fc6f0cc9f9bd03c81ce8ebfbf3feda99bdd6
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2022-01-29 17:43:41 +0530

    stack

--- Largest Rectangle in Histogram.java
+++ Largest Rectangle in Histogram.java
@@ -1,25 +1,22 @@
-class Solution{
-    public int largestRectangleArea(int heights[]){
-        if(heights.length == 0){
-            return 0;
-        }
-        Stack<Integer> stack = new Stack<Integer>();
-        int area = 0;
-        int maximumArea = Integer.MIN_VALUE;
-        stack.push(heights[0]);
-        for(int i=1; i<heights.length; i++){
-            if(stack.peek() <= heights[i]){
-                stack.push(i);
-            }else {
-                if(!stack.isEmpty()){
-                    int top = stack.pop();
-                    area = heights[top] * (stack.peek() -i - 1);
-                }else {
-                    area = heights[top];
-                }
-                maximumArea = Math.max(area, maximumArea);
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(-1);
+        
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            while (stack.peek() != -1 && heights[stack.peek()] >= heights[i]) {
+                max = Math.max(max, heights[stack.pop()] * (i - stack.peek() - 1));
             }
+            
+            stack.push(i);
         }
-        return maximumArea;
+        
+        while (stack.peek() != -1) {
+            max = Math.max(max, heights[stack.pop()] * (n - stack.peek() - 1));
+        }
+        
+        return max;
     }
 }
