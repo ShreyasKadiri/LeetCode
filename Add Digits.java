bd2d0006d9839fe3eeaa7cd06372233d451792ee
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2022-02-08 23:29:01 +0530

    Create Add Digits.java

---
+++ Add Digits.java
@@ -0,0 +1,13 @@
+class Solution{
+    public int addDigits(int num) {
+        if(num/10 == 0){
+             return num;
+        }
+        int sum=0;
+        while(num>0){
+            sum+= num % 10;
+            num/= 10;
+        }
+        return addDigits(sum);
+    }
+}
