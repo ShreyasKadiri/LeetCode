774fd89b10fa8630de993457d58e498204355b57
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2022-02-22 18:50:28 +0530

    ascii

---
+++ excel sheet column number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int titleToNumber(String s) {
+        int total = s.charAt(0)-'A' + 1;
+        for (int i=1; i<s.length(); i++) {
+            int value=s.charAt(i)-'A'+1;
+            total=total*26+value;
+        }
+        return total;
+    }   
+}
